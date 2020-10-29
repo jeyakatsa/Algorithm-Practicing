56bee05e6c3ee308bddeb19c00ff60409e182e02
Jeremy Akatsa <jeyakatsa@gmail.com>
2020-10-28 18:47:03 -0700

    Working on it

--- Arrays-I/To-Do-1.js
+++ Arrays-I/To-Do-1.js
@@ -1,29 +1,50 @@
 // Push Front
-// Given an array and an additional value, insert this value at the beginning of the array. Do this without using any built-in array methods.
+// Given an array and an additional value, insert this value at the beginning of the array. 
+// Do this without using any built-in array methods.
+var pushFrontArray = [1,2,3,4,5,6];
 
 
 
 // Pop Front
-// Given an array, remove and return the value at the beginning of the array. Do this without using any built-in array methods except pop().
+// Given an array, remove and return the value at the beginning of the array. 
+// Do this without using any built-in array methods except pop().
+var popFrontArray = [1,2,3,4,5,6];
 
 
 
 // Insert At
-// Given an array, index, and additional value, insert the value into array at given index. Do this without using built-in array methods. You can think of pushFront(arr,val) as equivalent to insertAt(arr,0,val).
+// Given an array, index, and additional value, insert the value into array at given index. 
+// Do this without using built-in array methods. 
+// You can think of pushFront(arr,val) as equivalent to insertAt(arr,0,val).
+var insertArray =  [1,2,3,4,5,6];
+var insertVal = 1;
+var insertIndex = insertArray.indexOf(3);
 
 
 
 // Remove At
-// Given an array and an index into array, remove and return the array value at that index. Do this without using built-in array methods except pop(). Think of popFront(arr) as equivalent to removeAt(arr,0).
+// Given an array and an index into array, remove and return the array value at that index. 
+//Do this without using built-in array methods except pop(). Think of popFront(arr) as 
+// equivalent to removeAt(arr,0).
+var removeAtArray = [1,2,3,4,5,6];
+var removeAtIndex = removeAtArray.indexOf(3);
 
 
 
 // Swap Pairs
-// Swap positions of successive pairs of values of given array. If length is odd, do not change the final element. For [1,2,3,4], return [2,1,4,3]. For example, change input ["Brendan",true,42] to [true,"Brendan",42]. As with all array challenges, do this without using any built-in array methods.
+// Swap positions of successive pairs of values of given array. If length is odd,
+// do not change the final element. For [1,2,3,4], return [2,1,4,3]. For example,
+// change input ["Brendan",true,42] to [true,"Brendan",42]. As with all array challenges,
+// do this without using any built-in array methods.
+var swapPairsArray = [1,2,3,4,5,6];
 
 
 
 // Remove Duplicates
-// Sara is looking to hire an awesome web developer and has received applications from various sources. Her assistant alphabetized them but noticed some duplicates. Given a sorted array, remove duplicate values. Because array elements are already in order, all duplicate values will be grouped together. As with all these array challenges, do this without using any built-in array methods.
+// Sara is looking to hire an awesome web developer and has received applications from
+// various sources. Her assistant alphabetized them but noticed some duplicates.
+// Given a sorted array, remove duplicate values. Because array elements are already
+// in order, all duplicate values will be grouped together. As with all these array
+// challenges, do this without using any built-in array methods.
 
 // Second: Solve this without using any nested loops.
