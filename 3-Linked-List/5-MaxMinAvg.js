e811f48851b1e41b9ff5642ddff5e3dbef3c026d
Jeremy Akatsa <jeyakatsa@gmail.com>
2020-11-08 11:41:04 -0800

    Done with MaxMinAvg

---
+++ 3-Linked-List/5-MaxMinAvg.js
@@ -0,0 +1,39 @@
+class Node {
+    constructor(value) {
+        this.value=value;
+        this.next = null;
+    }
+}
+
+class SSL {
+    constructor() {
+        this.head = null;
+    }
+
+    // max(){
+
+    // }
+    // min(){
+
+    // }
+    // avg(){
+
+    // }
+    maxMinAvg() {
+    var sum = 0;
+    var max = this.head.value;
+    var min = this.head.value;
+    var runner = this.head;
+    while(runner) {
+        sum += runner.value;
+        if(runner.value > max) {
+            max = runner.value;
+        }
+        else if(runner.value < min) {
+            min = runner.value;
+        }
+        runner = runner.next
+    }
+    return `max: ${max}, min: ${min}, avg: ${sum/this.length()}`
+}
+}
