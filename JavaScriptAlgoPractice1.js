0461550e2e5f9ee32a9f02cfdd3b6af5cd38da03
Jeremy Akatsa <jeyakatsa@gmail.com>
2020-12-15 20:22:45 -0800

    Learning

--- JavaScriptAlgoPractice1.js
+++ JavaScriptAlgoPractice1.js
@@ -261,3 +261,16 @@ var wordBreak = function(s, wordDict) {
     }
     return starts.has(s.length)
 };
+
+//322. Coin Change
+var coinChange = function(coins, amount) {
+    const dp = Array(amount+1).fill(Infinity);
+    dp[0] = 0;
+    
+    for(let i = 1; i < dp.length; i++) {
+        for(let coin of coins) {
+            if(i-coin >= 0) dp[i] = Math.min(dp[i], dp[i-coin]+1);
+        }
+    }
+    return dp[amount] === Infinity ? -1 : dp[amount];
+};
