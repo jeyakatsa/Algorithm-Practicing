7cf8b18007a562638ccbce5c9dbc6b7c01408bd7
Jeremy Akatsa <jeyakatsa@gmail.com>
2020-12-20 15:59:59 -0800

    Pushing to the limit

--- JavaScriptAlgoPractice1.js
+++ JavaScriptAlgoPractice1.js
@@ -502,3 +502,60 @@ var isPalindrome = function(head) {
     }
     return traverse(head);
 };
+
+//445. Add Two Numbers II
+var addTwoNumbers = function(l1, l2) {
+    let stack1 = [];
+    let stack2 = [];
+    while(l1) {
+        stack1.push(l1.val);
+        l1 = l1.next;
+    }
+    while(l2) {
+        stack2.push(l2.val);
+        l2 = l2.next;
+    }
+    let l3 = new ListNode(0);
+    while(stack1.length || stack2.length) {
+        let sum = 0;
+        if(stack1.length) sum += stack1.pop();
+        if(stack2.length) sum += stack2.pop();
+        sum += l3.val;
+        l3.val = sum%10;
+        let head = new ListNode(Math.floor(sum/10));
+        head.next = l3;
+        l3 = head;
+    }
+    return (l3.val === 0) ? l3.next : l3;
+};
+
+//17. Letter Combinations of a Phone Number
+const letterCombinations = (digits) => {
+    if (digits == null || digits.length === 0) return [];
+  
+    const map = {
+      2: 'abc',
+      3: 'def',
+      4: 'ghi',
+      5: 'jkl',
+      6: 'mno',
+      7: 'pqrs',
+      8: 'tuv',
+      9: 'wxyz',
+    };
+  
+    const res = [];
+    const go = (i, s) => {
+      if (i === digits.length) {
+        res.push(s);
+        return;
+      }
+  
+      for (const c of map[digits[i]]) {
+        go(i + 1, s + c);
+      }
+    };
+  
+    go(0, '');
+    return res;
+  };
