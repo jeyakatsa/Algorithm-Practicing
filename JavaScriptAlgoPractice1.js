7f58faea6628b1827469fe1c55d30633e99d744c
Jeremy Akatsa <jeyakatsa@gmail.com>
2020-12-20 12:59:29 -0800

    one step at a time

--- JavaScriptAlgoPractice1.js
+++ JavaScriptAlgoPractice1.js
@@ -466,3 +466,21 @@ var mergeTwoLists = function(l1, l2) {
   }
 
   //138. Copy List with Random Pointer
+  var copyRandomList = function(head) {
+    if(!head) {
+      return null;
+    }
+    const clones = new Map();
+    let n = head;
+    while(n) {
+      clones.set(n, new Node(n.val));
+      n = n.next
+    }
+    n = head;
+    while(n) {
+      clones.get(n).next = clones.get(n.next) || null;
+      clones.get(n).random = clones.get(n.random) || null;
+      n = n.next
+    }
+    return clones.get(head);
+};
