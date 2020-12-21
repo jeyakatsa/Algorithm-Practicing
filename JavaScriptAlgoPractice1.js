0735183d7e9ccbdc5f5a1284ec45e1752a21e958
Jeremy Akatsa <jeyakatsa@gmail.com>
2020-12-20 18:15:13 -0800

    Step By Step

--- JavaScriptAlgoPractice1.js
+++ JavaScriptAlgoPractice1.js
@@ -559,3 +559,30 @@ const letterCombinations = (digits) => {
     go(0, '');
     return res;
   };
+
+  //124. Binary Tree Maximum Path Sum
+  var maxPathSum = function(root) {
+    var max = -Infinity // Initialize to a very small number to handle a path of negative values
+    getMaxSum(root) // Call our recursive fn to start the program
+    
+    return max // Once we have popped out of our recursive calls, `max` contains our maximum path sum
+    
+    function getMaxSum(tree) {
+      if (tree == null) { return 0 } // Termination condition
+      
+      const leftBranch = Math.max(0, getMaxSum(tree.left)) // calculate the root to leaf sum where root is the left node
+      const rightBranch = Math.max(0, getMaxSum(tree.right)) // calculate the root to leaf sum where root is the right node
+      const currentPath = leftBranch + tree.val + rightBranch  // Sum the path: left -> root -> right (leaf to leaf)
+      
+      max = Math.max(max, currentPath) // if the current path is greater than the previous value of `max`, update `max` to the current path sum
+      return tree.val + Math.max(leftBranch, rightBranch)
+    }
+  };
+
+  //98. Validate Binary Search Tree
+  var isValidBST = function(root, min=null, max=null) {
+    if (!root) return true;
+    if (min && root.val <= min.val) return false;
+    if (max && root.val >= max.val) return false;
+    return isValidBST(root.left, min, root) && isValidBST(root.right, root, max);
+};
