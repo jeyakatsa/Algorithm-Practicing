4aec5ed71cbbef150c6eb14cf467268217aa5d63
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-01-01 21:13:45 -0800

    Yes

---
+++ All-Algos/1/One.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class One {
+
+    public static void main(String[] args) {
+        Array numbers = new Array(3);
+        numbers.insert(10);
+        numbers.insert(20);
+        numbers.insert(30);
+        numbers.insert(40);
+        numbers.removeAt(0);
+        numbers.print();
+    }
+    
+}
