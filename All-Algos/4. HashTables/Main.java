b9bea0cee45f568bff8b0924a9aae34a4f939485
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-01-07 19:06:34 -0800

    Till tomorrow

--- All-Algos/4. HashTables/Main.java
+++ All-Algos/4. HashTables/Main.java
@@ -1,5 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "Bring It");
+        System.out.println(map);
+        //
         
     }
 }
---
+++ All-Algos/4. HashTables/RepeatCharacter.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class RepeatCharacter {
+    Map<Integer, String> map = new HashMap<>();
+    // A Green Apple
+    // Iterate through string
+    // Example: if string[target string] == string[any other string]
+    // return null, else if string[target string] !== string[any other string]
+    // return string, break.
+}
