b13c170166ebdadea0e547e856c52cea5f5f22a3
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-01-07 17:55:49 -0800

    Doing the do

--- All-Algos/3/3.2/Main.java
+++ All-Algos/3/3.2/Main.java
@@ -1,10 +1,15 @@
 public class Main {
+
     public static void main(String[] args) {
-        // Stack
-        // push
-        // pop
-        // peek
-        // isEmpty
-        // int[]
+        ArrayStack stack = new ArrayStack();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        System.out.println(stack);
+
+        // [10,20,0,0,0]
+        // count = 1
+        // push(20)
+        //array[count] = 20
     }
 }
