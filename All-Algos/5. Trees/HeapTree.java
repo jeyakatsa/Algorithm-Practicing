c5cf243652381bb2c11b12a35fcc25541fb7beec
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-01-15 20:58:11 -0800

    Yea

--- All-Algos/5. Trees/HeapTree.java
+++ All-Algos/5. Trees/HeapTree.java
@@ -1,50 +1,48 @@
 public class HeapTree {
     public static void main(String[] args) {
-        HeapTree tree = new HeapTree();
-        tree.insert(10);
-        tree.insert(20);
-        tree.insert(30);
+        var heap = new HeapTree();
+        heap.insert(10);
+        heap.insert(5);
+        heap.insert(1);
+        heap.insert(17);
+        System.out.println("Done");
         // Heap
         // int[]
         // insert(int)
         // remove()
     }
 
-    class HeapNode {
-        int value;
-        HeapNode leftChild;
-        HeapNode rightChild;
-        public int height;
+    private int[] items = new int[10];
+    private int size;
 
-        HeapNode (int value) {
-            this.value = value;
-            rightChild = null;
-            leftChild = null;
-        }
-
-        @Override
-        public String toString(){
-            return "Value=" + this.value;
-        }
-    }
-
-    HeapTree root;
-
-    //Recursively Insert
     public void insert(int value) {
-        root = insert(root, value);
+        if(isFull())
+            throw new IllegalStateException();
+
+        items[size++] = value;
+
+        bubbleUp();
     }
 
-    private HeapNode insert(HeapTree root, int value){
-        //Base Case (to pull out of recursive)
-        if(root == null)
-            return new HeapNode(value);
-        
-        if (value < root.value)
-            root.leftChild = insert(root.leftChild, value);
-        else
-            root.rightChild = insert(root.rightChild, value);
+    public boolean isFull() {
+        return size == items.length;
+    }
 
-        return root;    
+    private void bubbleUp() {
+        var index = size = 1;
+        while(index > 0 && items[index] > items[parent(index)]) {
+            swap(index, parent(index));
+            index = parent(index);
+        }
+    }
+
+    private int parent(int index) {
+        return (index - 1) / 2;
+    }
+
+    private void swap (int first, int second) {
+        var temp = items[first];
+        items[first] = items[second];
+        items[second] = temp;
     }
 }
