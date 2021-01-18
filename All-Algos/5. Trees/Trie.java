e12bf7485ae0603049a592271cf25e0abc3f8de7
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-01-17 17:20:43 -0800

    Tries

--- All-Algos/5. Trees/Trie.java
+++ All-Algos/5. Trees/Trie.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class Trie {
     public static void main(String[] args) {
         var trie = new Trie();
@@ -7,10 +9,10 @@ public class Trie {
     }
 
     public static int ALPHABET_SIZE = 26;
-    
+
     private class Node {
         private char value;
-        private Node[] children = new Node[ALPHABET_SIZE];
+        private HashMap<Character, Node> children = new HashMap<>;
         private boolean isEndOfWord;
 
         public Node(char value) {
@@ -28,10 +30,9 @@ public class Trie {
     public void insert(String word) {
         var current = root;
         for (var ch : word.toCharArray()){
-            var index = ch - 'a';
-            if (current.children[index] == null)
-                current.children[index] = new Node(ch);
-            current = current.children[index];    
+            if (current.children.get(ch) == null)
+                current.children.put(ch, new Node(ch));
+            current = current.children.get(ch);    
         }
         current.isEndOfWord = true;
     }
