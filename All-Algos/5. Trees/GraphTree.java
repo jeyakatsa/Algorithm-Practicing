cd314b4b4d275998ea11599af64519a3d5baadd6
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-01-22 19:49:55 -0800

    Don't be too hard on yourself..

--- All-Algos/5. Trees/GraphTree.java
+++ All-Algos/5. Trees/GraphTree.java
@@ -11,16 +11,15 @@ import java.util.Stack;
 public class GraphTree {
     public static void main (String[] args) {
         var graph = new GraphTree();
-        graph.addNode("X");
         graph.addNode("A");
         graph.addNode("B");
-        graph.addNode("P");
-        graph.addEdge("X", "A");
-        graph.addEdge("X", "B");
-        graph.addEdge("A", "P");
-        graph.addEdge("B", "P");
-        var list = graph.topologicalSort();
-        System.out.println(list);
+        graph.addNode("C");
+        graph.addNode("D");
+        graph.addEdge("A", "B");
+        graph.addEdge("B", "C");
+        graph.addEdge("C", "D");
+        graph.addEdge("D", "A");
+        System.out.println(graph.hasCycle());
     }
 
     private class Node {
