2e1ba56b3b58b3c37fc7a7e143ddd9b23da31675
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-01-27 18:57:17 -0800

    Yesterday was better than today

---
+++ All-Algos/6. Arrays Expanded/BinarySearch.java
@@ -0,0 +1,13 @@
+public class BinarySearch {
+    public static void main(String[] args){
+        int[] numbers = {1,5,9,0,3,2,1};
+        var search = new BinarySearch();
+        var index = search.binarySearch(numbers, 2);
+        System.out.println(index);
+    }
+
+    public int binarySearch(int[] array, int number) {
+        return - 1;
+    }
+    
+}
