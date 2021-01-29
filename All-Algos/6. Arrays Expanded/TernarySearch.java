ca7c6760172b10da06a5d16b0b61918875e9d70b
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-01-28 18:11:41 -0800

    Started Ternary Search

---
+++ All-Algos/6. Arrays Expanded/TernarySearch.java
@@ -0,0 +1,19 @@
+public class TernarySearch {
+    public static void main (String[] args) {
+        int[] numbers = {1,2,3,4,5,6,7};
+        var search = new TernarySearch();
+        var index = search.ternarySearch(numbers, 6);
+        System.out.println(index);
+    }
+
+    public int ternarySearch (int[] array, int target) {
+        return ternarySearch(array, target, 0, array.length - 1);
+
+    }
+
+    private int ternarySearch (
+        int[] array, int target, int left, int right) {
+            return - 1;
+        }
+    
+}
