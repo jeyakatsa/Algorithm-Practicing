bccabf8c0bf0bb501832ef8437c8943a03a3d283
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-01-28 17:57:26 -0800

    Binary Search Done

--- All-Algos/6. Arrays Expanded/BinarySearch.java
+++ All-Algos/6. Arrays Expanded/BinarySearch.java
@@ -2,8 +2,27 @@ public class BinarySearch {
     public static void main(String[] args){
         int[] numbers = {1,3,5,6,7};
         var search = new BinarySearch();
-        var index = search.binarySearchRec(numbers, 7);
+        var index = search.binarySearch(numbers, 7);
         System.out.println(index);
+    }
+
+    public int binarySearch(int[] array, int target) {
+        var left = 0;
+        var right = array.length - 1;
+
+        while(left <= right) {
+            var middle = (left + right) / 2;
+
+            if(array[middle] == target)
+                return middle;
+
+            if (target < array[middle])
+                right = middle - 1;
+            else
+                left = middle + 1;        
+        }
+
+        return - 1;
     }
 
     public int binarySearchRec(int[] array, int target) {
