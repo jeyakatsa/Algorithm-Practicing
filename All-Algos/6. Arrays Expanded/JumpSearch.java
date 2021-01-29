8e36ff22a9227669438b82880da9c3b739ade1d9
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-01-28 19:23:33 -0800

    Better than yesterday

---
+++ All-Algos/6. Arrays Expanded/JumpSearch.java
@@ -0,0 +1,29 @@
+public class JumpSearch {
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3,4,5,6,7};
+        var search = new JumpSearch();
+        var index = search.jumpSearch(numbers, 2);
+        System.out.println(index);
+
+    }
+ 
+    public int jumpSearch(int[] array, int target) {
+        int blockSize = (int) Math.sqrt(array.length);
+        int start = 0;
+        int next = blockSize;
+
+        while(start < array.length && 
+        array[next - 1] < target) {
+            start = next;
+            next += blockSize;
+            if (next > array.length)
+                next = array.length;
+        }
+
+        for (var i = start; i < next; i++)
+            if (array[i] == target)
+                return i;
+
+        return -1;
+    }
+}
