a0db7e88bdc44208428cc9088b7cd44bc56d0ee3
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-01-29 17:34:54 -0800

    Started Exponential Search

---
+++ All-Algos/6. Arrays Expanded/ExponentialSearch.java
@@ -0,0 +1,12 @@
+public class ExponentialSearch {
+    public static void main (String[] args) {
+        int[] numbers = {1,2,3,4,5,6,7};
+        var search = new ExponentialSearch();
+        var index = search.exponentialSearch(numbers, 9);
+        System.out.println(index);
+    }
+
+    public int exponentialSearch(int[] array, int target) {
+        return - 1;
+    }
+}
