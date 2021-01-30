6253ac539610a2cb6bd0a2edd9851107c2160415
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-01-30 13:32:19 -0800

    What a terrible day...

--- All-Algos/6. ArraysExpanded/StringManipulation/StringManipulation.java
+++ All-Algos/6. ArraysExpanded/StringManipulation/StringManipulation.java
@@ -1,15 +1,23 @@
 public class StringManipulation {
     public static void main(String[] args) {
-        String[] words = {"hello"};
-        var vowels = new StringManipulation();
-        var vowelsCount = vowels.vowels(words, "o");
-        System.out.println(vowelsCount);
+        int count = StringManipulation.vowels("Hello World");
+        System.out.println(count);
     }
 
     //1- Find the number of vowels in a string. Vowels in English are A, E, O, U and I. 
     //Input: “hello”
     //Output: 2 
-    public String vowels(String[] array, String target) {
-        return null;
+    public static int vowels(String str) {
+        if (str == null)
+            return 0;
+
+        int count = 0;
+        String vowels = "aeiou";
+
+        for(var ch : str.toLowerCase().toCharArray())
+            if (vowels.indexOf(ch) != -1)
+                count++;
+
+        return count;        
     }
 }
