ecb81e7544ae40d2cb3b97b0e566ce5330d6a198
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-02-04 17:10:30 -0800

    Started

--- All-Algos/Practice2/Arrays/MergeSorted.java
+++ All-Algos/Practice2/Arrays/MergeSorted.java
@@ -1,20 +1,8 @@
 public class MergeSorted {
     public static int[] mergeArrays(int[] arr1, int[] arr2){
-        //create array3
-        //loop through array1, push numbers into array3
-        //loop through array2, push numbers into array3
-        int[] arr3 = {};
-
-        for (int i = 0; i < arr1.length; i++) {
-            arr3.push(arr1[i]);
-        }
-
-        for (int i = 0; i < arr2.length; i++) {
-            arr3.push(arr2[i]);
-        }
-
-
-        return arr3;
+        int s1 = arr1.length;
+        int s2 = arr2.length;
+        int 
     }
 
 
