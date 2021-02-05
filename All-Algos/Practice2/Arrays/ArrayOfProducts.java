80cf8674baa4a9324da2efdc1358defc729b33f9
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-02-04 20:01:57 -0800

    Not understanding 90% of this stuff is just funny as hell.

---
+++ All-Algos/Practice2/Arrays/ArrayOfProducts.java
@@ -0,0 +1,14 @@
+public class ArrayOfProducts {
+    public static int[] findProduct(int[] arr) {
+        int[] result = {0};
+        //set up target integer in array
+        //continue looping through array
+        //calculate other integers in array except for target integer
+        //return sum of calculated integers
+        //so if target i = 0, add sum of i-- >
+
+        //possibly loop forward, then loop backward?
+
+        return result;
+    }
+}
