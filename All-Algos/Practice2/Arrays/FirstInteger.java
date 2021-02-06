91a1a6a01ae8c8b33c05284ac65b29824c615b7a
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-02-05 18:06:50 -0800

    Working Solution.

--- All-Algos/Practice2/Arrays/FirstInteger.java
+++ All-Algos/Practice2/Arrays/FirstInteger.java
@@ -1,18 +1,35 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class FirstInteger {
     public static int findFirstUnique(int[] arr){
-        int result = arr[0];
-        //loop through array with i
-        //make inner loop through array with j
-        //if integer i != j, return and break
-        //O(n^2), intuitive approach
-        for(var i = 0; i < arr.length; i++){
-            for(var j = 0; j < arr.length; j++) {
-                if(arr[i] != arr[j]){
-                    result = arr[i];
-                }
+        // int result = arr[0];
+        // //loop through array with i
+        // //make inner loop through array with j
+        // //if integer i != j, return and break
+        // //O(n^2), intuitive approach
+        // for(var i = 0; i < arr.length; i++){
+        //     for(var j = 0; j < arr.length; j++) {
+        //         if(arr[i] != arr[j]){
+        //             result = arr[i];
+        //         }
+        //     }
+        // }
+        // return result;
+
+        Map<Integer, Integer> countElements = new HashMap<>();
+        for(int i = 0; i < arr.length; i++) {
+            countElements.put(arr[i],0);
+        }
+        for(int i = 0; i < arr.length; i++) {
+            countElements.put(arr[i], countElements.get(arr[i])+1);
+        }
+        for(int i = 0; i < arr.length; i++) {
+            if(countElements.get(arr[i]) <= 1) {
+                return arr[i];
             }
         }
-        return result;
+        return - 1;
     }
 
     public static void main(String[] args) {
