2e9097d2f4504de352d14deb35fa971a5f66f433
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-02-05 20:28:29 -0800

    My Hodge Podge Solution

--- All-Algos/Practice2/Arrays/RearrangePosNeg.java
+++ All-Algos/Practice2/Arrays/RearrangePosNeg.java
@@ -1,12 +1,28 @@
 public class RearrangePosNeg {
 
     public static void reArrange(int[] arr){
-
+        //if i < 0, .push to newArr
+        int[] newArr = {};
+        for(var i = 0; i < arr.length; i++) {
+            if(arr[i] > 0){
+                newArr.push(arr[i]);
+            }
+        }
+        //if i > 0 .push to newArr
+        for(var i = 0; i < arr.length; i++){
+            if(arr[i] < 0){
+                newArr.push(arr[i]);
+            }
+        }
     }
 
     public static void main(String[] args){
         int[] arr = {-1,-2,-3,-4,5,7,8,9,0,-7};
         reArrange(arr);
+        System.out.println("Array after Rearrangement: ");
+        for (int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
     }
     
 }
