b6889b42509b1f2949f3185e64cb502dbcd673d5
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-02-05 18:13:57 -0800

    Next

--- All-Algos/Practice2/Arrays/FirstInteger.java
+++ All-Algos/Practice2/Arrays/FirstInteger.java
@@ -8,15 +8,33 @@ public class FirstInteger {
         // //make inner loop through array with j
         // //if integer i != j, return and break
         // //O(n^2), intuitive approach
-        // for(var i = 0; i < arr.length; i++){
-        //     for(var j = 0; j < arr.length; j++) {
-        //         if(arr[i] != arr[j]){
-        //             result = arr[i];
-        //         }
-        //     }
+    //Inside Inner Loop Check Each index of outerLoop If it's repeated in array
+    //If it's not repeated then return this as first unique Integer
+            // boolean isRepeated = false;
+
+            // for (int i = 0; i < arr.length; i++) {
+
+            // for (int j = 0; j < arr.length; j++) {
+
+            //     if (arr[i] == arr[j] && i != j) {
+            //     isRepeated = true;
+            //     break;
+            //     }
+            // } //end of InnerLoop
+
+            // if (isRepeated == false) {
+            //     return arr[i];
+            // }
+            // else {
+            //     isRepeated = false;
+            // }
+            
+            // } //end of OuterLoop
+            // return - 1;
         // }
         // return result;
 
+        //O(n) approach.
         Map<Integer, Integer> countElements = new HashMap<>();
         for(int i = 0; i < arr.length; i++) {
             countElements.put(arr[i],0);
