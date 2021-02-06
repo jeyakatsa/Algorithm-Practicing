6db601cb3998aafe15dab8b9213a895f357f2119
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-02-06 11:57:09 -0800

    New Revelations...

--- All-Algos/Practice2/SinglyLinkedList/DeleteByValue.java
+++ All-Algos/Practice2/SinglyLinkedList/DeleteByValue.java
@@ -18,11 +18,31 @@ public class DeleteByValue<T> {
     }
 
     public void deleteByValue(T data) {
+        //if empty then simply return
         if(isEmpty()){
             return;
         }
 
-        
+        //Start from head node
+        Node currentNode = this.headNode;
+        Node prevNode = null; //previous node starts from null
+
+        if(currentNode.data.equals(data)) {
+            //data is at head so delete from head
+            deleteAtHead();
+            return;
+        }
+
+        //traverse the list searching for the data to delete
+        while(currentNode != null) {
+            //node to delete is found
+            if (data.equals(currentNode.data)) {
+                prevNode.nextNode = currentNode.nextNode;
+                return;
+            }
+            prevNode = currentNode;
+            currentNode = currentNode.nextNode;
+        }
     }
 
 
