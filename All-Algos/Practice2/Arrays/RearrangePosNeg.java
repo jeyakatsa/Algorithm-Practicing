84e354c3af5f0bfd0e5436fd91122776765d3dc0
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-02-05 20:34:31 -0800

    Yes?

--- All-Algos/Practice2/Arrays/RearrangePosNeg.java
+++ All-Algos/Practice2/Arrays/RearrangePosNeg.java
@@ -1,18 +1,25 @@
 public class RearrangePosNeg {
 
     public static void reArrange(int[] arr){
+
+        int[] newArr = new int[arr.length];
+        int newArray_index = 0;
+
         //if i < 0, .push to newArr
-        int[] newArr = {};
         for(var i = 0; i < arr.length; i++) {
             if(arr[i] > 0){
-                newArr.push(arr[i]);
+                newArr[newArray_index++] = arr[i];
             }
         }
         //if i > 0 .push to newArr
         for(var i = 0; i < arr.length; i++){
             if(arr[i] < 0){
-                newArr.push(arr[i]);
+                newArr[newArray_index++] = arr[i];
             }
+        }
+
+        for(int j = 0; j < newArr.length; j++){
+            arr[j] = newArr[j];
         }
     }
 
