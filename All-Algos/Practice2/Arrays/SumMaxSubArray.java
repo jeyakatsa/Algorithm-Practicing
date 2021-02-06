9a6a56c4904e870bd2d1457d1be57c1607702bed
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-02-05 21:56:07 -0800

    Don't forget to Check Test-Cases

---
+++ All-Algos/Practice2/Arrays/SumMaxSubArray.java
@@ -0,0 +1,29 @@
+public class SumMaxSubArray {
+    public static int findMaxSumSubArray(int[] arr) {
+        if (arr.length < 1) {
+            return 0;
+        }
+
+        int currMax = arr[0];
+        int globalMax = arr[0];
+        int lengtharray = arr.length;
+        for (int i = 1; i < lengtharray; i++) {
+            if (currMax < 0) {
+                currMax = arr[i];
+            } else {
+                currMax += arr[i];
+            }
+
+            if(globalMax < currMax) {
+                globalMax = currMax;
+            }
+        }
+        return globalMax;
+    }
+
+    public static void main (String[] args) {
+        int[] arr = {1,2,3,4,-1,-9,5,10};
+        int result = findMaxSumSubArray(arr);
+        System.out.println(result);
+    }
+}
