aeb863b6429d3c32d3225da7688c94feb10e36d1
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-02-08 18:57:28 -0800

    No Pain... No Gain.

--- All-Algos/Practice2/SinglyLinkedList/SinglyLinkedList.java
+++ All-Algos/Practice2/SinglyLinkedList/SinglyLinkedList.java
@@ -346,4 +346,23 @@ public class SinglyLinkedList<T> {
         }    
         return result;
     }
+
+    public static <T> Object nthElementFromEnd(SinglyLinkedList<T> list, int n) {
+
+        int size = list.getSize();
+        n = size - n + 1; // we can use the size variable to calculate distance from start
+        if (size == 0 || n > size) {
+            return null; //returns null if list is empty or n is greater than size
+        }
+        SinglyLinkedList<T>.Node current = list.getHeadNode();
+        int count = 2;
+        //traverse until count is not equal to n
+        while(current != null) {
+            if(count == n)
+                return current.data;
+            count++;
+            current = current.nextNode;    
+        }
+        return null;
+    }
 }
