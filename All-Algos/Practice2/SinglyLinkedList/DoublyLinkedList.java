c5028186ecabad1079cc022bf0a05581e875a19b
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-02-11 17:23:52 -0800

    Need to come back to palindrome Doubly Linked List

--- All-Algos/Practice2/SinglyLinkedList/DoublyLinkedList.java
+++ All-Algos/Practice2/SinglyLinkedList/DoublyLinkedList.java
@@ -1,11 +1,12 @@
 public class DoublyLinkedList<T> {
     public static void main(String[] args){
-        DoublyLinkedList<Integer> dll = new DoublyLinkedList<>();
-        for(int i = 1; i <= 10; i++) {
-            dll.insertAtHead(i);
-        }
+        // DoublyLinkedList<Integer> dll = new DoublyLinkedList<>();
+        // for(int i = 1; i <= 10; i++) {
+        //     dll.insertAtHead(i);
+        // }
 
-        System.out.println(dll.length());
+        // System.out.println(dll.length());
+
         // System.out.print("Original ");
         // dll.printList();
         // System.out.print("After deleting 10 ");
@@ -17,6 +18,23 @@ public class DoublyLinkedList<T> {
         // System.out.print("After deleting 5 ");
         // dll.deleteByValue(5);
         // dll.printList();
+
+        DoublyLinkedList<Integer> list1 = new DoublyLinkedList<Integer>();
+        list1.insertAtEnd(1);
+        list1.insertAtEnd(2);
+        list1.insertAtEnd(2);
+        list1.insertAtEnd(1);
+        System.out.print("1st ");
+        list1.printList();
+        DoublyLinkedList<Integer> list2 = new DoublyLinkedList<Integer>();
+        list2.insertAtEnd(6);
+        list2.insertAtEnd(8);
+        list2.insertAtEnd(6);
+        list2.insertAtEnd(6);
+        System.out.print("2nd ");
+        list2.printList();
+        System.out.println("Is 1st list a palindrome?  " + isPalindrome(list1));
+        System.out.println("Is 2nd list a palindrome?  " + isPalindrome(list2));
     }
 
 
@@ -27,16 +45,30 @@ public class DoublyLinkedList<T> {
     }
 
     public Node headNode;
+    public Node tailNode;
     public int size;
 
     public DoublyLinkedList() {
         this.headNode = null;
+        this.tailNode = null;
     }
 
     //Checks if the list is empty
     public boolean isEmpty() {
         if(headNode == null) return true;
             return false;
+    }
+
+    public Node getHeadNode() {
+        return headNode;
+    }
+
+    public Node getTailNode() {
+        return tailNode;
+    }
+
+    public int getSize() {
+        return size;
     }
 
     public void insertAtHead(T data) {
@@ -51,6 +83,20 @@ public class DoublyLinkedList<T> {
             headNode.prevNode = newNode;
         this.headNode = newNode;
         size++;    
+    }
+
+    public void insertAtEnd(T data) {
+        if(isEmpty()) {
+            insertAtHead(data);
+            return;
+        }
+        Node newNode = new Node();
+        newNode.data = data;
+        newNode.nextNode = null;
+        newNode.prevNode = tailNode;
+        tailNode.nextNode = newNode;
+        tailNode = newNode;
+        size++;
     }
 
     public void printList() {
@@ -70,6 +116,22 @@ public class DoublyLinkedList<T> {
         System.out.println(temp.data.toString() + " -> null");
     }
 
+    public void printListReverse() {
+        if(isEmpty()) {
+            System.out.println("List is Empty!");
+            return;
+        }
+
+        Node temp = tailNode;
+        System.out.print("List : null <-");
+
+        while(temp.prevNode != null) {
+            System.out.print(temp.data.toString() + " <-> ");
+            temp = temp.prevNode;
+        }
+        System.out.println(temp.data.toString() + " -> null");
+    }
+
     //deletes the first element
     public void deleteAtHead() {
         //if list is empty do nothing
@@ -80,6 +142,17 @@ public class DoublyLinkedList<T> {
         headNode = headNode.nextNode;
         headNode.prevNode = null;
         size--;   
+    }
+
+    public void deleteAtTail() {
+        if (isEmpty())
+            return;
+        tailNode = tailNode.prevNode;
+        if(tailNode == null)
+            headNode = null;
+        else
+            tailNode.nextNode = null;  
+        size--;          
     }
 
     public void deleteByValue(T data) {
@@ -122,6 +195,22 @@ public class DoublyLinkedList<T> {
         }
 
         return count;
+    }
+
+    public static <T> boolean isPalindrome(DoublyLinkedList<T> list) {
+        DoublyLinkedList<T>.Node start = list.getHeadNode(); // get the head pointer
+        DoublyLinkedList<T>.Node end = list.getTailNode(); // get tail
+        if (start == null) { // if list is empty, it is a palindrome
+            return true;
+        }
+        while(start != null) { //otherwise traverse list from both sides
+            if (start.data != end.data) {//if data mismatches at any point list is not a palindrome
+                return false;
+            }
+            start = start.nextNode;
+            end = end.prevNode;
+        }
+        return true; // if data doens't mismatcch any point, true.
     }
 
 
