d9de7826f13dc6b80d3ea5585ba696abf2072b8e
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-02-15 18:54:08 -0800

    Post Fix

--- All-Algos/Practice2/Stacks/Stack.java
+++ All-Algos/Practice2/Stacks/Stack.java
@@ -1,3 +1,4 @@
+
 public class Stack<V> {
     public static void main(String[] args) {
         Stack<Integer> stack = new Stack<Integer>(10);
@@ -13,17 +14,21 @@ public class Stack<V> {
         // }
 
         // System.out.println("\nIs Stack empty? \n" + stack.isEmpty());
-        stack.push(2);
-        stack.push(97);
-        stack.push(4);
-        stack.push(42);
-        stack.push(12);
-        stack.push(60);
-        stack.push(23);
-        sortStack(stack);
-        while(!stack.isEmpty()){
-            System.out.println(stack.pop());
-        }
+
+
+        // stack.push(2);
+        // stack.push(97);
+        // stack.push(4);
+        // stack.push(42);
+        // stack.push(12);
+        // stack.push(60);
+        // stack.push(23);
+        // sortStack(stack);
+        // while(!stack.isEmpty()){
+        //     System.out.println(stack.pop());
+        // }
+
+        System.out.println(evaluatePostFix("921*-8-4+"));
     }
 
 
@@ -105,6 +110,40 @@ public class Stack<V> {
         }
         while (!newStack.isEmpty())
             stack.push(newStack.pop());
+    }
+
+    public static int evaluatePostFix(String expression) {
+        Stack<Integer> stack = new Stack<>(expression.length());
+        //1.Scan expression character by character,
+        //2.If character is a number push it in stack
+        //3.If character is operator then pop two elements from stack
+        //perform the operation and put the result back in stack
+        //At the end, stack will contain result of whole expression.
+        for(int i = 0; i < expression.length(); i++) {
+            char character = expression.charAt(i);
+
+            if(!Character.isDigit(character)) {
+                Integer x = stack.pop();
+                Integer y = stack.pop();
+
+                switch (character) {
+                    case '+':
+                        stack.push(y + x);
+                        break;
+                    case '-':
+                        stack.push(y - x);
+                        break;
+                    case  '*':
+                        stack.push(y * x);
+                        break;
+                    case '/':
+                        stack.push(y / x);
+                        break;               
+                }
+            }else
+                stack.push(Character.getNumericValue(character));
+        }
+        return stack.pop();
     }
 
 }
