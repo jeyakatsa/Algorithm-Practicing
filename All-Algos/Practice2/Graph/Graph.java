86bbb7fafeb743fad960c91c3960e8d569569a67
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-02-18 17:50:26 -0800

    Graph Implementing

--- All-Algos/Practice2/Graph/Graph.java
+++ All-Algos/Practice2/Graph/Graph.java
@@ -1,4 +1,15 @@
 public class Graph {
+    public static void main(String[] args) {
+        Graph g = new Graph(4);
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 3);
+        g.addEdge(2, 3);
+
+        g.printGraph();
+    }
+
+
     int vertices; //Total number of vertices in graph
     DoublyLinkedList<Integer> adjacencyList[]; //An array of linked lists to store adjacent vertices
 
@@ -12,8 +23,6 @@ public class Graph {
         }
     }
 
-    void printGraph(); //Prints Graph (Adjacency List)
-
     void addEdge(int source, int destination) {
         if (source < vertices && destination < vertices) {
             this.adjacencyList[source].insertAtEnd(destination);
@@ -22,4 +31,23 @@ public class Graph {
             //this.adjacencyList[destination].insertAtEnd(source);
         }
     } //Adds an Edge from source vertex to destination vertex
+
+    public void printGraph() {
+        System.out.println(">>Adjacency List of Directed Graph<<");
+        for(int i = 0; i < vertices; i++) {
+            if (adjacencyList[i] != null){
+                System.out.println("|" + "| => ");
+
+                DoublyLinkedList<Integer>.Node temp = adjacencyList[i].getHeadNode();
+                while (temp != null){
+                    System.out.print("[" + temp.data + "] ->");
+                    temp = temp.nextNode;
+                }
+                System.out.println("null");
+            }
+            else{
+                System.out.println("|" + i + "| =>" + "null");
+            }
+        }
+    }
 }
