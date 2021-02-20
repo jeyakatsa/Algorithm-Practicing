34c0592a0a5282dc973e46e2e42fafe747f9caab
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-02-20 13:05:25 -0800

    Graph woring it out

--- All-Algos/Practice2/Graph/Graph.java
+++ All-Algos/Practice2/Graph/Graph.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class Graph {
     public static void main(String[] args) {
-        Graph g = new Graph(4);
+        // Graph g = new Graph(4);
         // g.addEdge(0, 1);
         // g.addEdge(0, 2);
         // g.addEdge(1, 3);
@@ -20,12 +20,45 @@ public class Graph {
         // g2.printGraph();
         // System.out.println(detectCycle(g2));
 
-        g.addEdge(0, 1);
-        g.addEdge(1, 2);
-        g.addEdge(3, 0);
-        g.addEdge(3, 1);
+        // g.addEdge(0, 1);
+        // g.addEdge(1, 2);
+        // g.addEdge(3, 0);
+        // g.addEdge(3, 1);
+        // g.printGraph();
+        // System.out.println("Mother Vertex is: " + findMotherVertex(g));
+
+        Graph g = new Graph(9);
+        g.addEdge(0,2);
+        g.addEdge(0,5);
+        g.addEdge(2,3);
+        g.addEdge(2,4);
+        g.addEdge(5,3);
+        g.addEdge(5,6);
+        g.addEdge(3,6);
+        g.addEdge(6,7);
+        g.addEdge(6,8);
+        g.addEdge(6,4);
+        g.addEdge(7,8);
+        
+         
         g.printGraph();
-        System.out.println("Mother Vertex is: " + findMotherVertex(g));
+        System.out.println("Number of edges: " + numEdges(g));
+        System.out.println();
+    
+        Graph g2 = new Graph(7);
+        g2.addEdge(1,2);
+        g2.addEdge(1,3);
+        g2.addEdge(3,4);
+        g2.addEdge(3,5);
+        g2.addEdge(2,5);
+        g2.addEdge(2,4);
+        g2.addEdge(4,6);
+        g2.addEdge(4,5);
+        g2.addEdge(6,5);
+    
+    
+        g2.printGraph();
+        System.out.println("Number of edges: " + numEdges(g2));
     }
 
 
