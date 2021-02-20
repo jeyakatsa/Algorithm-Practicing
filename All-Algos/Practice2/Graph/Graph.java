23f0f8ee1dee2b1453b6c76af49617ed10ba07c1
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-02-20 12:50:02 -0800

    Step by Step, Day by Day

--- All-Algos/Practice2/Graph/Graph.java
+++ All-Algos/Practice2/Graph/Graph.java
@@ -282,4 +282,27 @@ public class Graph {
             }
         }    
     }
+
+    public static int numEdges(Graph g) {
+
+        //For undirected graph, just sum up the size of all the
+        //adjacency lists for each vertex and then divide it by 2.
+        //It will give us total number ot edges in graph.
+        int sum = 0;
+
+        for (int i = 0; i < g.vertices; i++) {
+
+            DoublyLinkedList<Integer>.Node temp = null;
+            if (g.adjacencyList[i] != null)
+                temp = g.adjacencyList[i].headNode;
+            
+            while(temp != null) {
+                sum++;
+                temp = temp.nextNode;
+            }    
+        }
+        return sum/2;
+    }
+
+
 }
