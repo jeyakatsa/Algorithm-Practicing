d7d44404d2a23f75d6cf93bbf1014ab6b6ae19f9
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-02-19 17:28:23 -0800

    Complex

--- All-Algos/Practice2/Graph/Graph.java
+++ All-Algos/Practice2/Graph/Graph.java
@@ -1,12 +1,23 @@
 public class Graph {
     public static void main(String[] args) {
-        Graph g = new Graph(4);
+        Graph g = new Graph(5);
         g.addEdge(0, 1);
         g.addEdge(0, 2);
         g.addEdge(1, 3);
         g.addEdge(2, 3);
-
+        System.out.println("Graph1:");
         g.printGraph();
+        System.out.println("BFS traversal of Graph1 : " + bfs(g));
+        System.out.println();
+
+        Graph g2 = new Graph(5);
+        g2.addEdge(0, 1);
+        g2.addEdge(0, 4);
+        g2.addEdge(1, 2);
+        g2.addEdge(3, 4);
+        System.out.println("Graph1: ");
+        g2.printGraph();
+        System.out.println("BFS traversal of Graph1 : " + bfs(g2));
     }
 
 
@@ -54,8 +65,54 @@ public class Graph {
     //Breadth First Traversal of Graph g
     public static String bfs(Graph g) {
         String result = "";
-        //Checking
+        //Checking if the graph has no vertices
+        if (g.vertices < 1) {
+            return result;
+        }
 
+        //Boolean Array to hold the history of visited nodes (by default-false)
+        boolean[] visited = new boolean[g.vertices];
+
+        for(int i = 0; i < g.vertices; i++) {
+            //Checking whether the node is visited or not
+            if(!visited[i]){
+                result = result + bfsVisit(g, i, visited);
+            }
+        }
+        return result;
+    }
+    
+    public static String bfsVisit(Graph g, int source, boolean[] visited) {
+        String result  = "";
+
+        //Create Queue for Breadth First Traversal and enqueue source in it
+        Queue<Integer> queue = new Queue<>(g.vertices);
+
+        queue.enqueue(source);
+        visited[source] = true;
+
+        //Traverse while queue is not empty
+        while (!queue.isEmpty()) {
+
+            //Dequeue a vertex/node from queue and add it to result
+            int current_node = queue.dequeue();
+
+            result += String.valueOf(current_node);
+
+            //Get adjacent vertices to the current_node from the array,
+            //and if they are not already visited then enqueue them in Queue
+            DoublyLinkedList<Integer>.Node temp = null;
+            if(g.adjacencyList[current_node] != null)
+                temp = g.adjacencyList[current_node].headNode;
+
+            while(temp != null) {
+                if(!visited[temp.data]) {
+                    queue.enqueue(temp.data);
+                    visited[temp.data] = true; //Visit the current Node
+                }
+                temp = temp.nextNode;
+            }    
+        }
         return result;
     }
 }
