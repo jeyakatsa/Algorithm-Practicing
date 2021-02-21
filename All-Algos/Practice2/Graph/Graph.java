a9832e4e54ed692bd79c8cb8633e95eedabd241f
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-02-20 16:06:54 -0800

    Solve Mysoginy

--- All-Algos/Practice2/Graph/Graph.java
+++ All-Algos/Practice2/Graph/Graph.java
@@ -3,29 +3,13 @@ import java.util.Arrays;
 public class Graph {
     public static void main(String[] args) {
 
-        Graph g = new Graph(5);
+        Graph g=new Graph(5);
         g.addEdge(0,1);
         g.addEdge(0,2);
-        g.addEdge(0,3);
-        g.addEdge(3,4);
-        g.printGraph();
-        System.out.println("isTree : " + isTree(g));
-
-        Graph g2 = new Graph(4);
-        g2.addEdge(0,1);
-        g2.addEdge(0,2);
-        g2.addEdge(0,3);
-        g2.addEdge(3,2);
-        g2.printGraph();
-        System.out.println("isTree : " + isTree(g2));
-
-        Graph g3 = new Graph(6);
-        g3.addEdge(0,1);
-        g3.addEdge(0,2);
-        g3.addEdge(0,3);
-        g3.addEdge(4,5);
-        g3.printGraph();
-        System.out.println("isTree : " + isTree(g3));
+        g.addEdge(1,3); 
+        g.addEdge(3,4); 
+        g.addEdge(1,4);
+        System.out.println(findMin(g, 0, 2));
     }
 
 
@@ -396,5 +380,53 @@ public class Graph {
         return false;
     }
 
+    //Find Length of Shorted Path.
+    public static int findMin(Graph g, int source, int destination) {
+        //for loop to traverse BFS
+        //Max-value has to be integrated to check if vertices 
+        if (source == destination) {
+            return 0;
+        }
 
+        int num_of_vertices = g.vertices;
+
+        //Boolean Array to hold the history of visited nodes (by default-false)
+        //Make a node visited whenever you enqueue it into queue
+        boolean[] visited = new boolean[num_of_vertices];
+
+        //For keeping track of distance of current_node from source
+        int[] distance = new int[num_of_vertices];
+
+        //Create Queue for Breadth First Traversal and enqueue source in it
+        Queue<Integer> queue = new Queue<Integer>(num_of_vertices);
+
+        queue.enqueue(source);
+        visited[source] = true;
+
+        //Traverse while queue is not empty
+        while (!queue.isEmpty()) {
+            //dequeue a vertex/node from queue
+            int current_node = queue.dequeue();
+
+            //Get adjacent vertices to the current_node from the array,
+            //and if they are not already visited then enqueue them in the Queue
+            //and also update their distance from source by adding 1 in current_nodes's distance
+            DoublyLinkedList<Integer>.Node temp = null;
+            if(g.adjacencyList[current_node] != null)
+                temp = g.adjacencyList[current_node].headNode;
+
+            while (temp != null) {
+                if(!visited[temp.data]) {
+                    queue.enqueue(temp.data);
+                    visited[temp.data] = true;
+                    distance[temp.data] = distance[current_node] + 1;
+                }
+                if (temp.data == destination) {
+                    return distance[destination];
+                }
+                temp = temp.nextNode;
+            }    
+        }   
+        return -1;
+    }
 }
