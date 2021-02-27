7ab511ea13f977cde9da43a74d04965f05acbda2
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-02-27 09:42:45 -0800

    Heiroglyphs

--- All-Algos/Practice2/Tries/Trie.java
+++ All-Algos/Practice2/Tries/Trie.java
@@ -132,6 +132,72 @@ public class Trie {
         return false;    
     }
 
+    //Helper Function to return true if currentNode does not have any children
+    private boolean hasNoChildren(TrieNode currentNode) {
+        for (int i = 0; i < currentNode.children.length; i++){
+            if((currentNode.children[i]) != null)
+                return false;
+        }
+        return true;
+    }
+
+    //Recursive function to delete given key
+    private boolean deleteHelper(String key, TrieNode currentNode, int length, int level) {
+        
+        boolean deletedSelf = false;
+
+        if(currentNode == null) {
+            System.out.println("Key does not exist");
+            return deletedSelf;
+        }
+
+        //Base Case: If we have reached at the node which points to the alphabet at the end of the key.
+        if (level == length) {
+            //If there are no nodes ahead of this node in this path
+            //Then we can delete this node
+            if(hasNoChildren(currentNode)) {
+                currentNode = null;
+                deletedSelf = true;
+            }
+             //If there are nodes ahead of currentNode in this path
+            //Then we cannot delete currentNode. We simply unmark this as leaf
+            else {
+                currentNode.unMarkAsLeaf();
+                deletedSelf = false;
+            } 
+        }
+        else
+        {
+            TrieNode childNode = currentNode.children[getIndex(key.charAt(level))];
+            boolean childDeleted = deleteHelper(key, currentNode, length, level + 1);
+            if (childDeleted)
+            {
+                //Making children pointer also null: since child is deleted
+                currentNode.children[getIndex(key.charAt(level))] = null;
+                //If currentNode is leaf node that means currntNode is part of another key
+                //and hence we cannot delete this node and it's parent path nodes
+                if (currentNode.isEndWord) {
+                    deletedSelf = false;
+                }
+                //If childNode is deleted but if currentNode has more children then
+                //currentNode must be part of another key.
+                //So, we cannot delete currentNode
+                else if (!hasNoChildren(currentNode)){
+                    deletedSelf = false;
+                }
+                //else we can delete currentNode
+                else {
+                    currentNode = null;
+                    deletedSelf = true;
+                }
+            }
+            else {
+                deletedSelf = false;
+            }
+        }
+        return deletedSelf;
+    }
+
     public void delete (String key) {
         if ((root == null) || (key == null)) {
             System.out.println("Null key or Empty trie error");
