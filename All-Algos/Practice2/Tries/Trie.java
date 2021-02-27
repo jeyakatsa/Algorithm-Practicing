79ca4ece0ebab08d59cf8512f9cf2d43be375ce3
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-02-26 20:53:22 -0800

    Inserting Character

--- All-Algos/Practice2/Tries/Trie.java
+++ All-Algos/Practice2/Tries/Trie.java
@@ -4,21 +4,72 @@ public class Trie {
 
     }
 
-    Trie[] children;
-    boolean isEndWord; //Will be true if the node represents the end of word
-    static final int AlPHABET_SIZE = 26; // Total # of English Alphabets = 26
+    public class TrieNode {
+        TrieNode[] children;
+        boolean isEndWord; //Will be true if the node represents the end of word
+        static final int AlPHABET_SIZE = 26; // Total # of English Alphabets = 26
+    
+        //Constructor & Node
+        TrieNode() {
+            this.isEndWord = false;
+            this.children = new TrieNode[AlPHABET_SIZE];
+        }
+    
+        //function to mark the current Node as Leaf
+        public void markAsLeaf(){
+            this.isEndWord = true;
+        }
+    
+        //function to mark the currentNode as Leaf
+        public void unMarkAsLeaf() {
+            this.isEndWord = false;
+        }
+
+    }
+
+    private TrieNode root;
+
     Trie() {
-        this.isEndWord = false;
-        this.children = new Trie[AlPHABET_SIZE];
+        root = new TrieNode();
     }
 
-    //function to mark the current Node as Leaf
-    public void markAsLeaf(){
-        this.isEndWord = true;
+    //Function to get the index of a character 't'
+    public int getIndex(char t) {
+        return t - 'a';
     }
 
-    //function to mark the currentNode as Leaf
-    public void unMarkAsLeaf() {
-        this.isEndWord = false;
+    //Function to insert a key, value pair in the Trie
+    public void insert(String key, int value) {
+        if(key == null) {
+            System.out.println("Null Key can not be Inserted!");
+            return;
+        }
+        key = key.toLowerCase(); //Keys are stored in lowercase
+        TrieNode currentNode = this.root;
+        int index = 0; //to store character index
+
+        //Iterate the Trie with given character index,
+        //If nyll, create TrieNode go down level
+        for (int level = 0; level < key.length(); level++) {
+            index = getIndex(key.charAt(level));
+            if(currentNode.children[index] == null) {
+                currentNode.children[index] = new TrieNode();
+            }
+            currentNode = currentNode.children[index];
+        }
+        //Mark the end character as leaf node
+        currentNode.markAsLeaf();
+
     }
+
+    //function to search given key in Trie
+    public boolean search(String key) {
+        return false;
+    }
+
+    //function to delete
+    public boolean delete(String key) {
+        return false;
+    }
+
 }
