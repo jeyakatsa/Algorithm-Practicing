7327a1ee974da12c6cdb47e7fee91fa880437634
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-02-27 13:26:31 -0800

    Learning

--- All-Algos/Practice2/Tries/Trie.java
+++ All-Algos/Practice2/Tries/Trie.java
@@ -7,15 +7,16 @@ public class Trie {
         String keys[] = {"the", "a", "there", "answer", "any", "by",
          "bye", "their","abc"};
         String output[] = {"Not present in trie", "Present in trie"}; 
+
         Trie t = new Trie();
 
         System.out.println("Keys: " + Arrays.toString(keys) + "\n");
 
-        //Construct trie
-        int i;
-        for(i = 0; i < keys.length; i++) {
-            t.insert(keys[i]);
-        }
+        // //Construct trie
+        // int i;
+        // for(i = 0; i < keys.length; i++) {
+        //     t.insert(keys[i]);
+        // }
 
         // // Search for different keys and delete if found
         // if(t.search("the") == true)
@@ -49,10 +50,15 @@ public class Trie {
 
         // System.out.println(totalWords(t.getRoot()));
 
-        ArrayList<String> list = sortArray(keys);
-        for (i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i));
-        }
+        // ArrayList<String> list = sortArray(keys);
+        // for (i = 0; i < list.size(); i++) {
+        //     System.out.println(list.get(i));
+        // }
+
+        if(isFormationPossible(keys, "helloworld"))
+            System.out.println("true");
+        else
+            System.out.println("false");    
 
     }
 
@@ -274,7 +280,24 @@ public class Trie {
         return result;
     }
 
+    public static boolean isFormationPossible(String[] dict, String word) {
+        if(word.length() < 2 || dict.length < 2) {
+            return false;
+        }
 
+        //Create Trie and insert dictionary elements in it
+        Trie trie = new Trie();
 
+        for(int i = 0; i < word.length(); i++) {
+            //Slice the word into two strings in each iteration
+            String first = word.substring(0, i);
+            String second = word.substring(i, word.length());
 
+            //If both substrings are present in the trie, the condition is good
+            if(trie.search(first) && trie.search(second)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
