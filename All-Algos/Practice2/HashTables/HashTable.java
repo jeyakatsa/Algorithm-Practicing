3d36a56ad66dced9fd305732c1e7bfd7e9d7c539
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-03-01 18:27:45 -0800

    Hash Table

--- All-Algos/Practice2/HashTables/HashTable.java
+++ All-Algos/Practice2/HashTables/HashTable.java
@@ -1,11 +1,20 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class HashTable {
     public static void main (String[] args){
+        int[] arr1 = {9, 4, 7, 1, -2, 6, 5};
+        int[] arr2 = {7, 1, -2};
+        int[] arr3 = {10, 12};
+    
+        System.out.println(isSubset(arr1, arr2));
+        System.out.println(isSubset(arr1, arr3));
 
     }
 
+    //Class to represent entire hash table
     class HashEntry {
+        //to store chains of slots
         String key;
         int value;
 
@@ -65,6 +74,7 @@ public class HashTable {
             }
             head = head.next;
         }
+
         //Inserts key in the chain
         size++;
         head = bucket.get(b_Index);
@@ -146,7 +156,21 @@ public class HashTable {
         return head.value;    
     }
 
+    public static boolean isSubset(int[] arr1, int[] arr2) {
+        HashSet<Integer> hset = new HashSet<>();
+        //hset stores all the values of arr1
+        for(int i = 0; i < arr1.length; i++) {
+            if(!hset.contains(arr1[i]))
+                hset.add(arr1[i]);
+        }
 
-
+        //loop to check if all elements of arr2 also
+        //lies in arr1
+        for(int i = 0; i < arr2.length; i++) {
+            if(!hset.contains(arr2[i]))
+                return false;
+        }
+        return false;
+    }
 
 }
