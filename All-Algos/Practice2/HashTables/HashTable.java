520db8b3546142a597b4cccaf190a0d5fcc661a5
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-03-02 17:56:51 -0800

    Still Trying to fully understand

--- All-Algos/Practice2/HashTables/HashTable.java
+++ All-Algos/Practice2/HashTables/HashTable.java
@@ -1,14 +1,12 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class HashTable {
     public static void main (String[] args){
-        int[] arr1 = {9, 4, 7, 1, -2, 6, 5};
-        int[] arr2 = {7, 1, -2};
-        int[] arr3 = {10, 12};
-    
-        System.out.println(isDisjoint(arr1, arr2));
-        System.out.println(isDisjoint(arr1, arr3));
+        int[][] arr = {{1, 2}, {3, 4}, {5, 9}, {4, 3}, {9, 5}};
+        String symmetric = findSymmetric(arr);
+        System.out.println(symmetric);
 
     }
 
@@ -189,6 +187,31 @@ public class HashTable {
                 return false;
         } 
         return true;
+    }
+
+    public static String findSymmetric(int [][] arr) {
+        //Create an empty Hash Map
+        //Traverse given Array
+        //Look for second element of each pair in the hash. i.e for pair (1,2) look for key 2 in map.
+        //If found then store it in the result array, otherwise insert the pair in hash
+        HashMap<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+
+        String result = "";
+
+        for(int i = 0; i < arr.length; i++) {
+            int first = arr[i][0];
+            int second = arr[i][1];
+
+            Integer value = hashMap.get(second);
+
+            if(value != null && value == first) {
+                //Symetric Pair found
+                result += "{" + String.valueOf(second) + "," + String.valueOf(first) + "}"; 
+            }
+            else
+                hashMap.put(first, second);
+        }
+        return result;
     }
 
 }
