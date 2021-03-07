43c928c70d9cf337b9a9f968a19d69fea16a36d8
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-03-06 20:53:50 -0800

    Quick Sort

--- All-Algos/Practice3/Arrays/Arrays.java
+++ All-Algos/Practice3/Arrays/Arrays.java
@@ -86,7 +86,9 @@ public class Arrays {
         //     }
         // }
 
-        mergeSort(arr, 0, arr.length);
+        // mergeSort(arr, 0, arr.length);
+
+        quickSort(arr, 0, arr.length);
 
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
@@ -156,5 +158,37 @@ public class Arrays {
         }
         System.arraycopy(input, i, input, start + tempIndex, mid - i);
         System.arraycopy(temp, 0, input, start, tempIndex);
+    }
+
+    //Quick Sort O(nlogn)
+    public static void quickSort(int[] input, int start, int end) {
+        if(end - start < 2) {
+            return;
+        }
+
+        int pivotIndex = partition(input, start, end);
+        quickSort(input, start, pivotIndex);
+        quickSort(input, pivotIndex + 1, end);
+    }
+    public static int partition(int[] input, int start, int end) {
+        int pivot = input[start];
+        int i = start;
+        int j = end;
+
+        while(i < j) {
+            while (i < j && input[--j] >= pivot);
+            if(i < j) {
+                input[i] = input[j];
+            }
+
+            while (i<j && input[++i] <= pivot);
+            if (i < j) {
+                input[j] = input[i];
+            }
+        }
+
+        input[j] = pivot;
+        return j;
+
     }
 }
