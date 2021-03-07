13151f58af2dc2babb1d84d4bade888c57e81951
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-03-06 18:17:58 -0800

    Factorial

--- All-Algos/Practice3/Arrays/Arrays.java
+++ All-Algos/Practice3/Arrays/Arrays.java
@@ -103,4 +103,27 @@ public class Arrays {
         arr[i] = arr[j];
         arr[j] = temp;
     }
+
+    //Recursive Factorial
+    public static int recursiveFactorial (int num) {
+        if (num == 0) {
+            return 1;
+        }
+
+        return num * recursiveFactorial(num - 1);
+    }
+
+    //Iterative Factorial
+    public static int iterativeFactorial(int num) {
+        if(num == 0){
+            return 1;
+        }
+
+        int factorial = 1;
+        for (int i = 1; i <= num; i++){
+            factorial *= i;
+        }
+
+        return factorial;
+    }
 }
