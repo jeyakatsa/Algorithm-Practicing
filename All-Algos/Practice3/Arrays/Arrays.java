a4c01eb5251dd519a26cea4255bc970508f32848
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-03-06 22:04:58 -0800

    Counting Sort

--- All-Algos/Practice3/Arrays/Arrays.java
+++ All-Algos/Practice3/Arrays/Arrays.java
@@ -88,7 +88,9 @@ public class Arrays {
 
         // mergeSort(arr, 0, arr.length);
 
-        quickSort(arr, 0, arr.length);
+        // quickSort(arr, 0, arr.length);
+
+        countingSort(arr, 1, 10);
 
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
@@ -190,5 +192,22 @@ public class Arrays {
         input[j] = pivot;
         return j;
 
+    }
+
+    //Counting Sort O(n)
+    public static void countingSort(int[] input, int min, int max) {
+        int[] countArray = new int[(max - min) + 1];
+
+        for(int i = 0; i < input.length; i++) {
+            countArray[input[i] - min]++;
+        }
+
+        int j = 0;
+        for (int i = min; i <= max; i++) {
+            while (countArray[i - min] > 0) {
+                input[j++] = i;
+                countArray[i - min]--;
+            }
+        }
     }
 }
