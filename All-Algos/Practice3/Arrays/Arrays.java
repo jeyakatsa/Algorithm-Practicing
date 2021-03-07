4d2adf8086269806676cae4859cd659bd8d0e35d
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-03-06 16:21:05 -0800

    Insertion Sort

--- All-Algos/Practice3/Arrays/Arrays.java
+++ All-Algos/Practice3/Arrays/Arrays.java
@@ -39,18 +39,35 @@ public class Arrays {
         //     System.out.println(arr[i]);
         // }
 
-        //Selection Sort O(n^2)
-        for (int lastUnsortedIndex = arr.length - 1; lastUnsortedIndex > 0; 
-            lastUnsortedIndex--) {
-            int largest = 0;
-            for (int i = 0; i <= lastUnsortedIndex; i++) {
-                if(arr[i] > arr[largest]) {
-                    largest = i;
-                }
+        // //Selection Sort O(n^2)
+        // for (int lastUnsortedIndex = arr.length - 1; lastUnsortedIndex > 0; 
+        //     lastUnsortedIndex--) {
+        //     int largest = 0;
+        //     for (int i = 0; i <= lastUnsortedIndex; i++) {
+        //         if(arr[i] > arr[largest]) {
+        //             largest = i;
+        //         }
+        //     }
+        //     swap(arr, largest, lastUnsortedIndex);
+        // }
+        // for (int i = 0; i < arr.length; i++){
+        //     System.out.println(arr[i]);
+        // }
+
+        //Insertion Sort O(n^2)
+        for (int firstUnsortedIndex = 1; firstUnsortedIndex < arr.length;
+            firstUnsortedIndex++) {
+            int newElement = arr[firstUnsortedIndex];
+
+            int i;
+
+            for(i = firstUnsortedIndex; i > 0 && arr[i-1] > newElement; i--) {
+                arr[i] = arr[i -1];
             }
-            swap(arr, largest, lastUnsortedIndex);
+
+            arr[i] = newElement;
         }
-        for (int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
 
