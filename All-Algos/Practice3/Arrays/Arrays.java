b25371c1471d29bca1091e1b04d63a40a43cfc6c
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-03-08 18:13:01 -0800

    Radix Array

--- All-Algos/Practice3/Arrays/Arrays.java
+++ All-Algos/Practice3/Arrays/Arrays.java
@@ -90,10 +90,14 @@ public class Arrays {
 
         // quickSort(arr, 0, arr.length);
 
-        countingSort(arr, 1, 10);
+        // countingSort(arr, 1, 10);
 
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+        int[] radixArray = { 4246, 8875, 3252, 2746, 5059 };
+
+        radixSort(radixArray, 10, 4);
+
+        for (int i = 0; i < radixArray.length; i++) {
+            System.out.println(radixArray[i]);
         }
 
 
@@ -209,5 +213,40 @@ public class Arrays {
                 countArray[i - min]--;
             }
         }
+    }
+
+    //Radix Sort
+    public static void radixSort (int[] input, int radix, int width) {
+        for (int i = 0; i < width; i++) {
+            radixSingleSort(input, i, radix);
+        }
+    }
+    public static void radixSingleSort(int[] input, int position, int radix) {
+        int numItems = input.length;
+        int[] countArray = new int[radix];
+
+        for (int value: input) {
+            countArray[getDigit(position, value, radix)]++;
+        }
+        //Adjust count array
+        for (int j = 1; j < radix; j++) {
+            countArray[j] += countArray[j - 1];
+        }
+
+        int[] temp = new int[numItems];
+        for(int tempIndex = numItems - 1; tempIndex >= 0; tempIndex--) {
+            temp[--countArray[getDigit(position, input[tempIndex], radix)]]
+                = input[tempIndex]; 
+
+        }
+
+        for (int tempIndex = 0; tempIndex < numItems; tempIndex++) {
+            input[tempIndex] = temp[tempIndex];
+        }
+
+    }
+
+    public static int getDigit(int position, int value, int radix) {
+        return value / (int) Math.pow(radix, position) % radix;
     }
 }
