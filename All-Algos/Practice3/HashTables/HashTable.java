da8b0d8fdb17e6c9a716d95512cfbfb266fa6362
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-03-17 17:54:29 -0700

    Linear Probe

--- All-Algos/Practice3/HashTables/HashTable.java
+++ All-Algos/Practice3/HashTables/HashTable.java
@@ -5,13 +5,18 @@ public class HashTable {
         Employee janeJones = new Employee("Jane", "Jones", 5540);
         Employee samSmith = new Employee("Sam", "Smith", 4470);
         Employee beeDee = new Employee("Bee", "Dee", 1498);
+        Employee marySmith = new Employee("Mary", "Smith", 983);
+        Employee billEnd = new Employee("Bill", "End", 38);
 
         HashTable ht = new HashTable();
         ht.put("Jones", janeJones);
         ht.put("Smith", samSmith);
         ht.put("Dee", beeDee);
+        ht.put("Mary", marySmith);
 
         ht.printHashtable();
+
+        System.out.println("Retrieve key Dee: " + ht.get("Dee"));
 
     }
 
@@ -24,7 +29,20 @@ public class HashTable {
 
     public void put(String key, Employee employee) {
         int hashedKey = hashKey(key);
-        if (hashtable[hashedKey] != null) {
+        if (occupied(hashedKey)) {
+            int stopIndex = hashedKey;
+            if (hashedKey == hashtable.length - 1) {
+                hashedKey = 0;
+            }
+            else {
+                hashedKey++;
+            }
+
+            while (occupied(hashedKey) && hashedKey != stopIndex) {
+                hashedKey = (hashedKey + 1) % hashtable.length;
+            }
+        }
+        if (occupied(hashedKey)) {
             System.out.println("Sorry, there's already an employee at position " + hashedKey);
         }
         else {
@@ -39,6 +57,10 @@ public class HashTable {
 
     private int hashKey(String key) {
         return key.length() % hashtable.length;
+    }
+
+    private boolean occupied (int index) {
+        return hashtable[index] != null;
     }
 
     public void printHashtable() {
