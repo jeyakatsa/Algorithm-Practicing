6598cc78f1f5dd0ae74ea48785607f8b25008b68
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-04-01 17:41:40 -0700

    You learn the most from grueling failures and pain... Not from successes...

--- All-Algos/Practice4/1.SlidingWindow/Array.java
+++ All-Algos/Practice4/1.SlidingWindow/Array.java
@@ -21,7 +21,12 @@ public class Array {
         // System.out.print("Pair in array whose sum equals target: ["
         //  + result[0] + ", " + result[1] + "]");
 
-        System.out.println(Array.remove(sortedArr));
+        // System.out.println(Array.remove(sortedArr));
+
+        int[] result = Array.makeSquares(arr);
+        for (int num : result)
+            System.out.print(num + " ");
+        System.out.println();
     }
 
     //find contiguous subarrays of size '5' O(n * k)
@@ -132,104 +137,6 @@ public class Array {
         return maxLength;
     }
 
-    //Find pair in array whose given sum is targetSum
-    public static int[] search(int[] arr, int targetSum) {
-        //Base case addition incase arr has nothing. return exception argument
-        if (arr == null || arr.length == 0) {
-            throw new IllegalArgumentException();
-        }
-
-        // //MY SOLUTION: not optimal for array pairs far away from each other.
-        // //first pointer 's' to start at beginning of array
-        // int s = 0; 
-        // //2nd pointer 'x' to start at end of array
-        // int x = arr.length - 1;
-        // //for loop to loop through entire array
-        // //1st pointer loops first, 2nd pointer doesn't loop
-        // for (s = 0; s < arr.length; s++) {
-        //     //if sum of 1st pointer and 2nd pointer equals targetSum, 
-        //     if (targetSum == arr[s] + arr[x]) {
-        //         //else if sum of 1st pointer and 2nd pointer doesn't equal targetSum,
-        //         //.. decrement 2nd pointer and repeat If statement.
-        //         if (targetSum != arr[s] + arr[x]) {
-        //             x--;
-        //         }
-        //         return new int[] {arr[s], arr[x]};
-        //          //...return new integer with 1st pointer and 2nd pointer
-        //     }
-        // }
-        // //else return -1
-        // return new int[] { -1, -1 };
-
-        //Optimal Solution:
-        int left = 0, right = arr.length - 1;
-        while (left < right) {
-          int currentSum = arr[left] + arr[right];
-          if (currentSum == targetSum)
-            return new int[] { left, right }; // found the pair
     
-          if (targetSum > currentSum)
-            left++; // we need a pair with a bigger sum
-          else
-            right--; // we need a pair with a smaller sum
-        }
-        return new int[] { -1, -1 };
-    }
-
-    //Remove duplicates from an array of sorted numbers, return arr.length
-    public static int remove(int[] arr) {
-        //base case for if array has no numbers, return illegalargumentexception
-        //set left target (beginning of array) and right target (end of array)
-        //if left target == right target, pop(remove left target & right target)
-        //return arr.length
-        //else arr.length;
-
-        // MY SOLUTION: Wrong, no pop?
-        // if (arr == null || 0 == arr.length) {
-        //     throw new IllegalArgumentException();
-        // }
-        // int left = 0, right = arr.length - 1;
-        // for (left = 0; left < arr.length; left++) {
-        //     if (left == right) {
-        //         arr.pop(arr[left], arr[right]);
-        //         return arr.length;
-        //     }
-        //     if (left != right) {
-        //         right--;
-        //     }
-
-        // }
-        // return arr.length;
-
-        //OTHER SOLUTION: right
-        int nextNonDuplicate = 1; // index of the next non-duplicate element
-        for (int i = 1; i < arr.length; i++) {
-          if (arr[nextNonDuplicate - 1] != arr[i]) {
-            arr[nextNonDuplicate] = arr[i];
-            nextNonDuplicate++;
-          }
-        }
-    
-        return nextNonDuplicate;
-    }
-
-    //Given sorted Array, create new array containing squares of all numbers 
-    //in sorted order...
-    public static int[] makeSquares(int[] arr) {
-        //Obviously, base case...
-        //Then loop through entire array and square all numbers
-        //return all numbers into for loop.
-        //to temp variable that stores all numbers?
-        if (arr == null || 0 == arr.length) {
-            throw new IllegalArgumentException();
-        }
-        int squaredNum;
-        int[] squares = new int[arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            squaredNum = arr[i] * arr[i];
-            
-        }
-        return squares;
-    }
 
 }
