4e08c64439db525e685d96cacf04e3093a7c6cf5
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-04-09 17:51:33 -0700

    Next.

--- All-Algos/Practice4/4.MergeIntervals/LinkedList.java
+++ All-Algos/Practice4/4.MergeIntervals/LinkedList.java
@@ -13,8 +13,31 @@ class Interval {
 class MergeIntervals {
 
   public static List<Interval> merge(List<Interval> intervals) {
+    if (intervals.size() < 2)
+      return intervals;
+
+    // sort the intervals by start time
+    Collections.sort(intervals, (a, b) -> Integer.compare(a.start, b.start));
+
     List<Interval> mergedIntervals = new LinkedList<Interval>();
-    // TODO: Write your code here
+    Iterator<Interval> intervalItr = intervals.iterator();
+    Interval interval = intervalItr.next();
+    int start = interval.start;
+    int end = interval.end;
+
+    while (intervalItr.hasNext()) {
+      interval = intervalItr.next();
+      if (interval.start <= end) { // overlapping intervals, adjust the 'end'
+        end = Math.max(interval.end, end);
+      } else { // non-overlapping interval, add the previous interval and reset
+        mergedIntervals.add(new Interval(start, end));
+        start = interval.start;
+        end = interval.end;
+      }
+    }
+    // add the last interval
+    mergedIntervals.add(new Interval(start, end));
+
     return mergedIntervals;
   }
 
