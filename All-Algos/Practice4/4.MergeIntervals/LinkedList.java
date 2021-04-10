240140c5308199917d267c53eb066d3b4af2a92a
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-04-09 19:52:40 -0700

    Getting longer and longer

--- All-Algos/Practice4/4.MergeIntervals/LinkedList.java
+++ All-Algos/Practice4/4.MergeIntervals/LinkedList.java
@@ -43,10 +43,29 @@ class MergeIntervals {
 
   //Insert Interval
   public static List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+    if (intervals == null || intervals.isEmpty())
+      return Arrays.asList(newInterval);
+
     List<Interval> mergedIntervals = new ArrayList<>();
-    if (intervals.size() < 2) {
-      return intervals;
+
+    int i = 0;
+    // skip (and add to output) all intervals that come before the 'newInterval'
+    while (i < intervals.size() && intervals.get(i).end < newInterval.start)
+      mergedIntervals.add(intervals.get(i++));
+
+    // merge all intervals that overlap with 'newInterval'
+    while (i < intervals.size() && intervals.get(i).start <= newInterval.end) {
+      newInterval.start = Math.min(intervals.get(i).start, newInterval.start);
+      newInterval.end = Math.max(intervals.get(i).end, newInterval.end);
+      i++;
     }
+
+    // insert the newInterval
+    mergedIntervals.add(newInterval);
+
+    // add all the remaining intervals to the output
+    while (i < intervals.size())
+      mergedIntervals.add(intervals.get(i++));
 
     return mergedIntervals;
   }
