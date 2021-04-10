548284c696db31dc63810552aa9b6cdc777fe7b3
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-04-09 21:51:22 -0700

    Until tomorrow

--- All-Algos/Practice4/4.MergeIntervals/LinkedList.java
+++ All-Algos/Practice4/4.MergeIntervals/LinkedList.java
@@ -81,41 +81,70 @@ class MergeIntervals {
   //Intervals Intersection
   //Given two lists of intervals, find intersection of two lists.
   public static Interval[] merge2(Interval[] arr1, Interval[] arr2) {
-    List<Interval> intervalsIntersection = new ArrayList<Interval>();
-    if ((arr1 == null || arr1.length == 0) || (arr2 == null || arr2.length == 0)) {
-      return arr1 + arr2;
+    List<Interval> result = new ArrayList<Interval>();
+    int i = 0, j = 0;
+    while (i < arr1.length && j < arr2.length) {
+      // check if the interval arr[i] intersects with arr2[j]
+      // check if one of the interval's start time lies within the other interval
+      if ((arr1[i].start >= arr2[j].start && arr1[i].start <= arr2[j].end)
+          || (arr2[j].start >= arr1[i].start && arr2[j].start <= arr1[i].end)) {
+        // store the intersection part
+        result.add(new Interval(Math.max(arr1[i].start, arr2[j].start), Math.min(arr1[i].end, arr2[j].end)));
+      }
+
+      // move next from the interval which is finishing first
+      if (arr1[i].end < arr2[j].end)
+        i++;
+      else
+        j++;
     }
 
-    return intervalsIntersection.toArray(new Interval[intervalsIntersection.size()]);
+    return result.toArray(new Interval[result.size()]); 
   }
 
 
   public static void main(String[] args) {
-    List<Interval> input = new ArrayList<Interval>();
-    input.add(new Interval(1, 4));
-    input.add(new Interval(2, 5));
-    input.add(new Interval(7, 9));
-    System.out.print("Merged intervals: ");
-    for (Interval interval : MergeIntervals.merge(input))
+    Interval[] input1 = new Interval[] { new Interval(1, 3), new Interval(5, 6), new Interval(7, 9) };
+    Interval[] input2 = new Interval[] { new Interval(2, 3), new Interval(5, 7) };
+    Interval[] result = merge2(input1, input2);
+    System.out.print("Intervals Intersection: ");
+    for (Interval interval : result)
       System.out.print("[" + interval.start + "," + interval.end + "] ");
     System.out.println();
 
-    input = new ArrayList<Interval>();
-    input.add(new Interval(6, 7));
-    input.add(new Interval(2, 4));
-    input.add(new Interval(5, 9));
-    System.out.print("Merged intervals: ");
-    for (Interval interval : MergeIntervals.merge(input))
+    input1 = new Interval[] { new Interval(1, 3), new Interval(5, 7), new Interval(9, 12) };
+    input2 = new Interval[] { new Interval(5, 10) };
+    result = merge2(input1, input2);
+    System.out.print("Intervals Intersection: ");
+    for (Interval interval : result)
       System.out.print("[" + interval.start + "," + interval.end + "] ");
-    System.out.println();
 
-    input = new ArrayList<Interval>();
-    input.add(new Interval(1, 4));
-    input.add(new Interval(2, 6));
-    input.add(new Interval(3, 5));
-    System.out.print("Intervals aftr inserting new interval: ");
-    for (Interval interval : MergeIntervals.insert(input, new Interval(2, 9)))
-      System.out.print("[" + interval.start + "," + interval.end + "] ");
-    System.out.println();
+
+    // List<Interval> input = new ArrayList<Interval>();
+    // input.add(new Interval(1, 4));
+    // input.add(new Interval(2, 5));
+    // input.add(new Interval(7, 9));
+    // System.out.print("Merged intervals: ");
+    // for (Interval interval : MergeIntervals.merge(input))
+    //   System.out.print("[" + interval.start + "," + interval.end + "] ");
+    // System.out.println();
+
+    // input = new ArrayList<Interval>();
+    // input.add(new Interval(6, 7));
+    // input.add(new Interval(2, 4));
+    // input.add(new Interval(5, 9));
+    // System.out.print("Merged intervals: ");
+    // for (Interval interval : MergeIntervals.merge(input))
+    //   System.out.print("[" + interval.start + "," + interval.end + "] ");
+    // System.out.println();
+
+    // input = new ArrayList<Interval>();
+    // input.add(new Interval(1, 4));
+    // input.add(new Interval(2, 6));
+    // input.add(new Interval(3, 5));
+    // System.out.print("Intervals aftr inserting new interval: ");
+    // for (Interval interval : MergeIntervals.insert(input, new Interval(2, 9)))
+    //   System.out.print("[" + interval.start + "," + interval.end + "] ");
+    // System.out.println();
   }
 }
