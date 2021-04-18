5fb5cefaf345c9249e7847fef8473edb05d4be37
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-04-17 21:41:56 -0700

    .

--- All-Algos/Practice4/6.ReversalLinkedList/LinkedList.java
+++ All-Algos/Practice4/6.ReversalLinkedList/LinkedList.java
@@ -27,23 +27,41 @@ public class LinkedList {
         return previous;    
     }
 
-    public static ListNode reverseSub(ListNode head) {
-        if(head == null) {
-            throw new IllegalArgumentException("No Numbers/Integers Available");
+    public static ListNode reverseSub(ListNode head, int p, int q) {
+        if (p == q)
+          return head;
+    
+        // after skipping 'p-1' nodes, current will point to 'p'th node
+        ListNode current = head, previous = null;
+        for (int i = 0; current != null && i < p - 1; ++i) {
+          previous = current;
+          current = current.next;
         }
-
-        ListNode current = head;
-        ListNode previous = null;
-        ListNode next = null;
-
-        while (current != null) {
-            next = current.next;
-            current.next = previous;
-            previous = current;
-            current = next;
+    
+        // we are interested in three parts of the LinkedList, part before index 'p', part between 'p' and 
+        // 'q', and the part after index 'q'
+        ListNode lastNodeOfFirstPart = previous; // points to the node at index 'p-1'
+        // after reversing the LinkedList 'current' will become the last node of the sub-list
+        ListNode lastNodeOfSubList = current;
+        ListNode next = null; // will be used to temporarily store the next node
+        // reverse nodes between 'p' and 'q'
+        for (int i = 0; current != null && i < q - p + 1; i++) {
+          next = current.next;
+          current.next = previous;
+          previous = current;
+          current = next;
         }
-
-        return previous;
+    
+        // connect with the first part
+        if (lastNodeOfFirstPart != null)
+          lastNodeOfFirstPart.next = previous; // 'previous' is now the first node of the sub-list
+        else // this means p == 1 i.e., we are changing the first node (head) of the LinkedList
+          head = previous;
+    
+        // connect with the last part
+        lastNodeOfSubList.next = current;
+    
+        return head;
     }
 
     public static void main(String[] args) {
@@ -54,13 +72,12 @@ public class LinkedList {
         head.next.next.next.next = new ListNode(10);
 
         ListNode result = reverse(head);
-        ListNode result2 = reverseSub(head);
-        while(result != null) {
-            System.out.print("Nodes of the reversed LinkedList are: ");
-            while(result != null) {
-                System.out.print(result.value + " ");
-                result = result.next;
-            }
+        
+        ListNode result2 = reverseSub(head, 2, 4);
+        System.out.print("Nodes of the reversed LinkedList are: ");
+        while(result2 != null) {
+            System.out.print(result2.value + " ");
+            result2 = result2.next;
         }
     }
 
