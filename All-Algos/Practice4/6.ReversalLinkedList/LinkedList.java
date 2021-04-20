58e35ac82df94f88f9a3241956f1f8c2e43ae00f
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-04-19 20:20:23 -0700

    Everyday, rain or shine, sleet or snow

--- All-Algos/Practice4/6.ReversalLinkedList/LinkedList.java
+++ All-Algos/Practice4/6.ReversalLinkedList/LinkedList.java
@@ -101,12 +101,55 @@ public class LinkedList {
     return head;
   }
 
+  //Reverse every other K element.
+  public static ListNode reverseOtherK(ListNode head, int k) {
+    if (head == null || k <= 1){
+      return head;
+    }
+
+    ListNode current = head;
+    ListNode previous = null;
+
+    while (current != null) {
+
+      ListNode lastNodeOfPreviousPart = previous;
+      ListNode lastNodeOfSubList = current;
+      ListNode next = null;
+
+      for (int i = 0; current != null && i < k; i++) {
+        next = current.next;
+        current.next = previous;
+        previous = current;
+        current = next;
+      }
+
+      if(lastNodeOfPreviousPart != null) {
+        lastNodeOfPreviousPart.next = previous;
+      }
+      else {
+        head = previous;
+      }
+
+      lastNodeOfSubList = current;
+
+      for (int i = 0; current != null && i < k; ++i) {
+        previous = current;
+        current = current.next;
+      }
+    }
+
+    return head;
+  }
+
   public static void main(String[] args) {
-    ListNode head = new ListNode(2);
-    head.next = new ListNode(4);
-    head.next.next = new ListNode(6);
-    head.next.next.next = new ListNode(8);
-    head.next.next.next.next = new ListNode(10);
+    ListNode head = new ListNode(1);
+    head.next = new ListNode(2);
+    head.next.next = new ListNode(3);
+    head.next.next.next = new ListNode(4);
+    head.next.next.next.next = new ListNode(5);
+    head.next.next.next.next.next = new ListNode(6);
+    head.next.next.next.next.next.next = new ListNode(7);
+    head.next.next.next.next.next.next.next = new ListNode(8);
 
     // ListNode result = reverse(head);
     
