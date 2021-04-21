2e5710ae62cf82fa972e77bd9daebeadf4679404
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-04-20 18:54:58 -0700

    lol

--- All-Algos/Practice4/6.ReversalLinkedList/LinkedList.java
+++ All-Algos/Practice4/6.ReversalLinkedList/LinkedList.java
@@ -143,6 +143,24 @@ public class LinkedList {
 
   //Rotate LinkedList
 
+  public static ListNode rotateLinkedList(ListNode head, int rotate) {
+    if (head == null || rotate <= 0) {
+      return head;
+    }
+
+    ListNode current = head;
+    ListNode previous = null;
+    ListNode next = null;
+
+    while (current != null) {
+      next = current.next;
+      current.next = previous;
+      previous = current;
+      current = next;
+
+    }
+  }
+
   public static void main(String[] args) {
     ListNode head = new ListNode(1);
     head.next = new ListNode(2);
