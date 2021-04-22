c3de6e1b4840ceb5b8a90dbffd4bed9af0be5b88
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-04-21 19:50:32 -0700

    Progress.

--- All-Algos/Practice4/7.BreadthSearch/BreadthFirst.java
+++ All-Algos/Practice4/7.BreadthSearch/BreadthFirst.java
@@ -10,8 +10,11 @@ class TreeNode {
     }
 }
 
-//Given a binary tree, populate an array to represent its level traversal
+
 class LevelOrderTraversal {
+
+    //Level Order Traversal
+    //Given a binary tree, populate an array to represent its level traversal
     public static List<List<Integer>> traverse(TreeNode root) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
         if (root == null)
@@ -23,20 +26,51 @@ class LevelOrderTraversal {
             int levelSize = queue.size();
             List<Integer> currentLevel = new ArrayList<>(levelSize);
             for (int i = 0; i < levelSize; i++) {
-            TreeNode currentNode = queue.poll();
-            // add the node to the current level
-            currentLevel.add(currentNode.val);
-            // insert the children of current node in the queue
-            if (currentNode.left != null)
-                queue.offer(currentNode.left);
-            if (currentNode.right != null)
-                queue.offer(currentNode.right);
+                TreeNode currentNode = queue.poll();
+                // add the node to the current level
+                currentLevel.add(currentNode.val);
+                // insert the children of current node in the queue
+                if (currentNode.left != null)
+                    queue.offer(currentNode.left);
+                if (currentNode.right != null)
+                    queue.offer(currentNode.right);
             }
             result.add(currentLevel);
         }
     
         return result;
     }
+
+    //Reverse Level Order Traversal
+    //Given binary tree, populate an array to represent its level in reverse order
+    public static List<List<Integer>> reverseOrder(TreeNode root){
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        //base case, if root equals null, return result
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>(levelSize);
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = queue.poll();
+                // add node to current level
+                currentLevel.add(currentNode.val);
+                //Insert the children of current node to the queue
+                if(currentNode.left != null)
+                    queue.offer(currentNode.left);
+                if(currentNode.right != null)
+                    queue.offer(currentNode.right);    
+            }
+            //Append current level at beginning
+            result.add(0, currentLevel);
+        }
+        return result;        
+
+    }
+
 
     public static void main(String[] args) {
         TreeNode root = new TreeNode(12);
@@ -45,7 +79,7 @@ class LevelOrderTraversal {
         root.left.left = new TreeNode(9);
         root.right.left = new TreeNode(10);
         root.right.right = new TreeNode(5);
-        List<List<Integer>> result = LevelOrderTraversal.traverse(root);
+        List<List<Integer>> result = LevelOrderTraversal.reverseOrder(root);
         System.out.println("Level order traversal: " + result);
     }        
 }
