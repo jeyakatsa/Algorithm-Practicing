3b0cbf32f94a31c07cd034ee47e6660f3c73df75
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-04-21 20:33:32 -0700

    Learn Improve

--- All-Algos/Practice4/7.BreadthSearch/BreadthFirst.java
+++ All-Algos/Practice4/7.BreadthSearch/BreadthFirst.java
@@ -68,7 +68,40 @@ class LevelOrderTraversal {
             result.add(0, currentLevel);
         }
         return result;        
+    }
 
+    //Zigzag Traversal
+    public static List<List<Integer>> zigZag(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        boolean leftToRight = true;
+        while(!queue.isEmpty()){
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>(levelSize);
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = queue.poll();
+
+                //add the node to the current level based on the traverse direction
+                if(leftToRight)
+                    currentLevel.add(currentNode.val);
+                else
+                    currentLevel.add(0, currentNode.val);
+
+                //insert the children of current node in the queue
+                if(currentNode.left != null)
+                    queue.offer(currentNode.left);
+                if(currentNode.right != null)
+                    queue.offer(currentNode.right);    
+            }
+            result.add(currentLevel);
+            //reverse traversal direction
+            leftToRight = !leftToRight;
+        }
+        return result;
     }
 
 
@@ -79,7 +112,9 @@ class LevelOrderTraversal {
         root.left.left = new TreeNode(9);
         root.right.left = new TreeNode(10);
         root.right.right = new TreeNode(5);
-        List<List<Integer>> result = LevelOrderTraversal.reverseOrder(root);
+        root.right.left.left = new TreeNode(20);
+        root.right.left.right = new TreeNode(17);
+        List<List<Integer>> result = LevelOrderTraversal.zigZag(root);
         System.out.println("Level order traversal: " + result);
     }        
 }
