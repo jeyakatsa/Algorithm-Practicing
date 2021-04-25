4be04ee262a2458056821020135be74ed27d8512
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-04-24 18:50:43 -0700

    .

--- All-Algos/Practice4/8.DepthSearch/DepthSearch.java
+++ All-Algos/Practice4/8.DepthSearch/DepthSearch.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 
 class TreeNode {
     int val;
@@ -10,6 +12,8 @@ class TreeNode {
   }
   
   class TreePathSum {
+
+    //Sum of Paths
     public static boolean hasPath(TreeNode root, int sum) {
         if (root == null) {
             return false;
@@ -22,6 +26,41 @@ class TreeNode {
         return hasPath(root.left, sum - root.val) ||
         hasPath(root.right, sum - root.val);
     }
+
+
+
+    //Find All Tree Paths
+    public static List<List<Integer>> findPaths (TreeNode root, int sum) {
+        List<List<Integer>> allPaths = new ArrayList<>();
+        List<Integer> currentPath = new ArrayList<>();
+        findPathsRecursive(root, sum, currentPath, allPaths);
+        return allPaths;
+    }
+    public static void findPathsRecursive(TreeNode currentNode, int sum, 
+    List<Integer> currentPath, List<List<Integer>> allPaths) {
+        if (currentNode == null) {
+            return;
+        }
+
+        //add current node to path
+        currentPath.add(currentNode.val);
+
+        //if current node is a leaf and its value is equal to sum, save path
+        if (currentNode.val == sum && currentNode.left == null
+        && currentNode.right == null) {
+            allPaths.add(new ArrayList<Integer>(currentPath));
+        } 
+        else {
+            //traverse left subtree
+            findPathsRecursive(currentNode.left, sum - currentNode.val,
+            currentPath, allPaths);
+            //traverse right subtree
+            findPathsRecursive(currentNode.right, sum - currentNode.val, 
+            currentPath, allPaths);
+        }
+
+        currentPath.remove(currentPath.size() - 1);
+    }
   
     public static void main (String[] args){
       TreeNode root = new TreeNode(12);
@@ -30,6 +69,9 @@ class TreeNode {
       root.left.left = new TreeNode(9);
       root.right.left = new TreeNode(10);
       root.right.right = new TreeNode(5);
-      System.out.println("Tree has path: " + hasPath(root, 23));
+      int sum = 23;
+      List<List<Integer>> result = findPaths(root, sum);
+      System.out.println("Tree paths with sum: " + sum + 
+      ": " + result);
     }
   }
