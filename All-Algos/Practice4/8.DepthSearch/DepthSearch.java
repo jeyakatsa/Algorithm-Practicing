d4422ab88509f6ef316871a75cf1c4bf47c24704
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-04-25 15:51:39 -0700

    .

--- All-Algos/Practice4/8.DepthSearch/DepthSearch.java
+++ All-Algos/Practice4/8.DepthSearch/DepthSearch.java
@@ -148,15 +148,48 @@ class TreePathSum {
         currentPath.remove(currentPath.size() - 1);
     
         return pathCount;
-    }    
+    }
+
+
+    //Tree Diameter
+    private static int treeDiameter = 0;
+    public static int findDiameter(TreeNode root) {
+        calculateHeight(root);
+        return treeDiameter;
+    }
+    public static int calculateHeight(TreeNode currentNode) {
+        if (currentNode == null) {
+            return -1;
+        }
+
+        int leftTreeHeight = calculateHeight(currentNode.left);
+        int rightTreeHeight = calculateHeight(currentNode.right);
+
+        // if the current node doesn't have a left or right subtree, we can't have
+        // a path passing through it, since we need a leaf node on each side
+        if (leftTreeHeight != 0 && rightTreeHeight != 0) {
+
+            // diameter at the current node will be equal to the height of left subtree +
+            // the height of right sub-trees + '1' for the current node
+            int diameter = leftTreeHeight + rightTreeHeight + 1;
+    
+            // update the global tree diameter
+            treeDiameter = Math.max(treeDiameter, diameter);
+        }
+    
+        // height of the current node will be equal to the maximum of the heights of
+        // left or right subtrees plus '1' for the current node
+        return Math.max(leftTreeHeight, rightTreeHeight) + 1;       
+    }
+
   
     public static void main (String[] args){
-      TreeNode root = new TreeNode(12);
-      root.left = new TreeNode(7);
-      root.right = new TreeNode(1);
-      root.left.left = new TreeNode(9);
-      root.right.left = new TreeNode(10);
-      root.right.right = new TreeNode(5);
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.right.left = new TreeNode(5);
+        root.right.right = new TreeNode(6);
     //   int sum = 23;
     //   List<List<Integer>> result = findPaths(root, sum);
     //   System.out.println("Tree paths with sum: " + sum + 
@@ -166,6 +199,14 @@ class TreePathSum {
 
     // System.out.println("Tree has path in Sequence: " + findSequence(root, new int[] {12, 7, 9}));
     
-    System.out.println("Tree has path: " + countPaths(root, 11));
+    // System.out.println("Tree has path: " + countPaths(root, 11));
+        System.out.println("Tree Diameter: " + findDiameter(root));
+        root.left.left = null;
+        root.right.left.left = new TreeNode(7);
+        root.right.left.right = new TreeNode(8);
+        root.right.right.left = new TreeNode(9);
+        root.right.left.right.left = new TreeNode(10);
+        root.right.right.left.left = new TreeNode(11);
+        System.out.println("Tree Diameter: " + findDiameter(root));    
     }
 }
