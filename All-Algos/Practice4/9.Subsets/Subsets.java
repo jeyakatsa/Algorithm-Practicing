a34671313b43d9c8384eec804da46a95fdc45f95
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-04-25 17:20:32 -0700

    work

--- All-Algos/Practice4/9.Subsets/Subsets.java
+++ All-Algos/Practice4/9.Subsets/Subsets.java
@@ -25,6 +25,37 @@ public class Subsets {
         return subsets;
     }
 
+    //Subsets With Duplicates
+    public static List<List<Integer>> findSubsetDuplicates(int[] nums) {
+        List<List<Integer>> subsets = new ArrayList<>();
+        if(nums == null) {
+            return subsets;
+        }
+        subsets.add(new ArrayList<>());
+        for (int currentNumber : nums) {
+            int n = subsets.size();
+            for (int i = 0; i < n; i++) {
+                List<Integer> set = new ArrayList<>(subsets.get(i));
+                set.add(currentNumber);
+                subsets.add(set);
+            }
+        }
+        return subsets;
+    }
+
+    public static List<List<Integer>> sub(int[] nums) {
+        List<List<Integer>> subsets = new ArrayList<>();
+        subsets.add(new ArrayList<>());
+        for(int currentNumber : nums) {
+            int n = subsets.size();
+            for(int i = 0; i < n; i++) {
+                List<Integer> set = new ArrayList<>(subsets.get(i));
+                set.add(currentNumber);
+                subsets.add(set);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         List<List<Integer>> result = Subsets.findSubsets(new int[] { 1, 3 });
         System.out.println("Here is the list of subsets: " + result);
