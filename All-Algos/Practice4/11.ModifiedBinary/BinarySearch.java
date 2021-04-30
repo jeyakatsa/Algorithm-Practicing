39c143b28deaaf3a5116a0aa4609fbead0e4fb23
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-04-30 16:50:36 -0700

    .

--- All-Algos/Practice4/11.ModifiedBinary/BinarySearch.java
+++ All-Algos/Practice4/11.ModifiedBinary/BinarySearch.java
@@ -92,6 +92,36 @@ public class BinarySearch {
     // since the loop is running until 'start <= end', so at the end of the while loop, 'start == end+1'
     return letters[start % n];
   }
+
+  //Number Range
+  public static int[] findRange(int[] arr, int key) {
+    int[] result = new int[] { -1, -1 };
+    result[0] = search(arr, key, false);
+    if (result[0] != -1) // no need to search, if 'key' is not present in the input array
+      result[1] = search(arr, key, true);
+    return result;
+  }
+
+  // modified Binary Search
+  private static int search(int[] arr, int key, boolean findMaxIndex) {
+    int keyIndex = -1;
+    int start = 0, end = arr.length - 1;
+    while (start <= end) {
+      int mid = start + (end - start) / 2;
+      if (key < arr[mid]) {
+        end = mid - 1;
+      } else if (key > arr[mid]) {
+        start = mid + 1;
+      } else { // key == arr[mid]
+        keyIndex = mid;
+        if (findMaxIndex)
+          start = mid + 1; // search ahead to find the last index of 'key'
+        else
+          end = mid - 1; // search behind to find the first index of 'key'
+      }
+    }
+    return keyIndex;
+  }
   
   public static void main(String[] args) {
     System.out.println(BinarySearch.agnosticBinarySearch(new int[] { 4, 6, 10 }, 10));
@@ -101,6 +131,6 @@ public class BinarySearch {
     System.out.println(NextLetter.searchNextLetter(new char[] { 'a', 'c', 'f', 'h' }, 'f'));
     System.out.println(NextLetter.searchNextLetter(new char[] { 'a', 'c', 'f', 'h' }, 'b'));
     System.out.println(NextLetter.searchNextLetter(new char[] { 'a', 'c', 'f', 'h' }, 'm'));
-    System.out.println(NextLetter.searchNextLetter(new char[] { 'a', 'c', 'f', 'h' }, 'h'));        
+    System.out.println(NextLetter.searchNextLetter(new char[] { 'a', 'c', 'f', 'h' }, 'h'));
   }
 }
