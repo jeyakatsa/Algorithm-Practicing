e75849017b7fee878e1f3e3299271865704d94eb
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-04-30 19:51:12 -0700

    Am I in over my head?

--- All-Algos/Practice4/11.ModifiedBinary/BinarySearch.java
+++ All-Algos/Practice4/11.ModifiedBinary/BinarySearch.java
@@ -211,7 +211,23 @@ public class BinarySearch {
 
     }
     return arr[arr.length - 1];
-  }  
+
+    //OTHER SOLUTION...
+    int start = 0, end = arr.length - 1;
+    while (start < end) {
+      int mid = start + (end - start) / 2;
+      if (arr[mid] > arr[mid + 1]) {
+        end = mid;
+      } else {
+        start = mid + 1;
+      }
+    }
+
+    // at the end of the while loop, 'start == end'
+    return arr[start];
+  }
+
+  //
   
   public static void main(String[] args) {
     System.out.println(BinarySearch.agnosticBinarySearch(new int[] { 4, 6, 10 }, 10));
