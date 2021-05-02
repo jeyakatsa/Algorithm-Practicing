5f6f11a9589524366112ca8de6dc9dd6377fe84d
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-05-02 13:52:05 -0700

    Cuba!

--- All-Algos/Practice4/13.HeapTopK/Heap.java
+++ All-Algos/Practice4/13.HeapTopK/Heap.java
@@ -19,6 +19,25 @@ public class Heap {
         // the heap has the top 'K' numbers, return them in a list
         return new ArrayList<>(minHeap);
     } 
+
+    public static int findKthSmallestNumber(int[] nums, int k) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>((n1, n2) -> n2 - n1);
+        // put first k numbers in the max heap
+        for (int i = 0; i < k; i++)
+          maxHeap.add(nums[i]);
+    
+        // go through the remaining numbers of the array, if the number from the array is smaller than the
+        // top (biggest) number of the heap, remove the top number from heap and add the number from array
+        for (int i = k; i < nums.length; i++) {
+          if (nums[i] < maxHeap.peek()) {
+            maxHeap.poll();
+            maxHeap.add(nums[i]);
+          }
+        }
+    
+        // the root of the heap has the Kth smallest number
+        return maxHeap.peek();
+      }  
     
     public static void main(String[] args) {
         List<Integer> result = findKLargestNumbers(new int[] { 3, 1, 5, 12, 2, 11 }, 3);
