0e8a736b9c3955aa96c5db41ba713a35a2ba1ffe
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-05-02 14:51:42 -0700

    Ok Ok

--- All-Algos/Practice4/13.HeapTopK/Heap.java
+++ All-Algos/Practice4/13.HeapTopK/Heap.java
@@ -37,7 +37,40 @@ public class Heap {
     
         // the root of the heap has the Kth smallest number
         return maxHeap.peek();
-      }  
+    }  
+
+    class Point {
+        int x;
+        int y;
+      
+        public Point(int x, int y) {
+          this.x = x;
+          this.y = y;
+        }
+      
+        public int distFromOrigin() {
+          // ignoring sqrt
+          return (x * x) + (y * y);
+        }
+      }
+    public static List<Point> findClosestPoints(Point[] points, int k) {
+        PriorityQueue<Point> maxHeap = new PriorityQueue<>((p1, p2) -> p2.distFromOrigin() - p1.distFromOrigin());
+        // put first 'k' points in the max heap
+        for (int i = 0; i < k; i++)
+          maxHeap.add(points[i]);
+    
+        // go through the remaining points of the input array, if a point is closer to the origin than the top point 
+        // of the max-heap, remove the top point from heap and add the point from the input array
+        for (int i = k; i < points.length; i++) {
+          if (points[i].distFromOrigin() < maxHeap.peek().distFromOrigin()) {
+            maxHeap.poll();
+            maxHeap.add(points[i]);
+          }
+        }
+    
+        // the heap has 'k' points closest to the origin, return them in a list
+        return new ArrayList<>(maxHeap);
+      }    
     
     public static void main(String[] args) {
         List<Integer> result = findKLargestNumbers(new int[] { 3, 1, 5, 12, 2, 11 }, 3);
