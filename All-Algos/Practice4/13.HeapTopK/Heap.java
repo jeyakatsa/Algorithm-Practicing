990c209af0584a2aff081eb07c038f9f72a683d1
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-05-05 17:49:01 -0700

    .

--- All-Algos/Practice4/13.HeapTopK/Heap.java
+++ All-Algos/Practice4/13.HeapTopK/Heap.java
@@ -184,44 +184,43 @@ public class Heap {
     //   return minHeap.peek();
     // }
 
-  public static List<Integer> findClosestElements(int[] arr, int K, Integer X) {
-    int index = binarySearch(arr, X);
-    int low = index - K, high = index + K;
-    low = Math.max(low, 0); // 'low' should not be less than zero
-    high = Math.min(high, arr.length - 1); // 'high' should not be greater the size of the array
+  // public static List<Integer> findClosestElements(int[] arr, int K, Integer X) {
+  //   int index = binarySearch(arr, X);
+  //   int low = index - K, high = index + K;
+  //   low = Math.max(low, 0); // 'low' should not be less than zero
+  //   high = Math.min(high, arr.length - 1); // 'high' should not be greater the size of the array
 
-    PriorityQueue<Entry> minHeap = new PriorityQueue<>((n1, n2) -> n1.key - n2.key);
-    // add all candidate elements to the min heap, sorted by their absolute difference from 'X'
-    for (int i = low; i <= high; i++)
-      minHeap.add(new Entry(Math.abs(arr[i] - X), i));
+  //   PriorityQueue<Entry> minHeap = new PriorityQueue<>((n1, n2) -> n1.key - n2.key);
+  //   // add all candidate elements to the min heap, sorted by their absolute difference from 'X'
+  //   for (int i = low; i <= high; i++)
+  //     minHeap.add(new Entry(Math.abs(arr[i] - X), i));
 
-    // we need the top 'K' elements having smallest difference from 'X'
-    List<Integer> result = new ArrayList<>();
-    for (int i = 0; i < K; i++)
-      result.add(arr[minHeap.poll().value]);
+  //   // we need the top 'K' elements having smallest difference from 'X'
+  //   List<Integer> result = new ArrayList<>();
+  //   for (int i = 0; i < K; i++)
+  //     result.add(arr[minHeap.poll().value]);
 
-    Collections.sort(result);
-    return result;
-  }
-
-  private static int binarySearch(int[] arr, int target) {
-    int low = 0;
-    int high = arr.length - 1;
-    while (low <= high) {
-      int mid = low + (high - low) / 2;
-      if (arr[mid] == target)
-        return mid;
-      if (arr[mid] < target) {
-        low = mid + 1;
-      } else {
-        high = mid - 1;
-      }
-    }
-    if (low > 0) {
-      return low - 1;
-    }
-    return low;
-  }
+  //   Collections.sort(result);
+  //   return result;
+  // }
+  // private static int binarySearch(int[] arr, int target) {
+  //   int low = 0;
+  //   int high = arr.length - 1;
+  //   while (low <= high) {
+  //     int mid = low + (high - low) / 2;
+  //     if (arr[mid] == target)
+  //       return mid;
+  //     if (arr[mid] < target) {
+  //       low = mid + 1;
+  //     } else {
+  //       high = mid - 1;
+  //     }
+  //   }
+  //   if (low > 0) {
+  //     return low - 1;
+  //   }
+  //   return low;
+  // }
 
   //Maximum Distinct Elements
   public static int findMaximumDistinctElements(int[] nums, int k) {
