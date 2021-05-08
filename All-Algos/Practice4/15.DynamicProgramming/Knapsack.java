ee163a52f2d36730decf07e04238ffabfaba3281
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-05-08 16:53:42 -0700

    .

--- All-Algos/Practice4/15.DynamicProgramming/Knapsack.java
+++ All-Algos/Practice4/15.DynamicProgramming/Knapsack.java
@@ -28,6 +28,36 @@ public class Knapsack {
         return Math.max(profit1, profit2);
     }
 
+    public boolean canPartition(int[] num) {
+        int sum = 0;
+        for (int i = 0; i < num.length; i++)
+          sum += num[i];
+    
+        // if 'sum' is a an odd number, we can't have two subsets with equal sum
+        if(sum % 2 != 0)
+          return false;
+    
+        return this.canPartitionRecursive(num, sum/2, 0);
+    }
+    private boolean canPartitionRecursive(int[] num, int sum, int currentIndex) {
+        // base check
+        if (sum == 0)
+            return true;
+
+        if(num.length == 0 || currentIndex >= num.length)
+            return false;
+
+        // recursive call after choosing the number at the currentIndex
+        // if the number at currentIndex exceeds the sum, we shouldn't process this
+        if( num[currentIndex] <= sum ) {
+            if(canPartitionRecursive(num, sum - num[currentIndex], currentIndex + 1))
+            return true;
+        }
+
+        // recursive call after excluding the number at the currentIndex
+        return canPartitionRecursive(num, sum, currentIndex + 1);
+    }    
+
     public static void main(String[] args) {
         Knapsack ks = new Knapsack();
         int[] profits = {1, 6, 10, 16};
@@ -36,5 +66,13 @@ public class Knapsack {
         System.out.println("Total knapsack profit ---> " + maxProfit);
         maxProfit = ks.solveKnapsack(profits, weights, 6);
         System.out.println("Total knapsack profit ---> " + maxProfit);
+
+        Knapsack ps = new Knapsack();
+        int[] num = {1, 2, 3, 4};
+        System.out.println(ps.canPartition(num));
+        num = new int[]{1, 1, 3, 4, 7};
+        System.out.println(ps.canPartition(num));
+        num = new int[]{2, 3, 4, 6};
+        System.out.println(ps.canPartition(num));        
     }    
 }
