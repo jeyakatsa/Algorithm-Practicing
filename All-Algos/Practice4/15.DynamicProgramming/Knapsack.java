e2c57479558982141b77c3bb9de87e4fd4afe581
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-05-08 17:57:20 -0700

    .

--- All-Algos/Practice4/15.DynamicProgramming/Knapsack.java
+++ All-Algos/Practice4/15.DynamicProgramming/Knapsack.java
@@ -56,7 +56,41 @@ public class Knapsack {
 
         // recursive call after excluding the number at the currentIndex
         return canPartitionRecursive(num, sum, currentIndex + 1);
-    }    
+    }
+    
+    public boolean canPartitionSum(int[] num, int sum) {
+        int n = num.length;
+        boolean[][] dp = new boolean[n][sum + 1];
+    
+        // populate the sum=0 columns, as we can always form '0' sum with an empty set
+        for (int i = 0; i < n; i++)
+          dp[i][0] = true;
+    
+        // with only one number, we can form a subset only when the required sum is
+        // equal to its value
+        for (int s = 1; s <= sum; s++) {
+          dp[0][s] = (num[0] == s ? true : false);
+        }
+    
+        // process all subsets for all sums
+        for (int i = 1; i < num.length; i++) {
+          for (int s = 1; s <= sum; s++) {
+            // if we can get the sum 's' without the number at index 'i'
+            if (dp[i - 1][s]) {
+              dp[i][s] = dp[i - 1][s];
+            } else if (s >= num[i]) {
+              // else include the number and see if we can find a subset to get the remaining
+              // sum
+              dp[i][s] = dp[i - 1][s - num[i]];
+            }
+          }
+        }
+    
+        // the bottom-right corner will have our answer.
+        return dp[num.length - 1][sum];
+    } 
+    
+    
 
     public static void main(String[] args) {
         Knapsack ks = new Knapsack();
@@ -73,6 +107,14 @@ public class Knapsack {
         num = new int[]{1, 1, 3, 4, 7};
         System.out.println(ps.canPartition(num));
         num = new int[]{2, 3, 4, 6};
-        System.out.println(ps.canPartition(num));        
+        System.out.println(ps.canPartition(num));  
+        
+        Knapsack ss = new Knapsack();
+        int[] numm = { 1, 2, 3, 7 };
+        System.out.println(ss.canPartitionSum(numm, 6));
+        numm = new int[] { 1, 2, 7, 1, 5 };
+        System.out.println(ss.canPartitionSum(numm, 10));
+        numm = new int[] { 1, 3, 4, 8 };
+        System.out.println(ss.canPartitionSum(numm, 6));        
     }    
 }
