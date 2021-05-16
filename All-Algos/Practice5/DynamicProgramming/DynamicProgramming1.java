10448ffd76fc3cc7be2e88c8e67c103d98188c89
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-05-15 21:51:28 -0700

    .

--- All-Algos/Practice5/DynamicProgramming/DynamicProgramming1.java
+++ All-Algos/Practice5/DynamicProgramming/DynamicProgramming1.java
@@ -35,5 +35,31 @@ public class DynamicProgramming1 {
             R++;
         }
         return R - L - 1;
-    }           
+    }      
+    
+    
+    //DECODE LETTERS TO NUMBERS
+    // Input: s = "226"
+    // Output: 3
+    // Explanation: "226" could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
+    public int numDecodings(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) != '0' ? 1 : 0;
+        for (int i = 2; i <= n; i++) {
+            int first = Integer.valueOf(s.substring(i - 1, i));
+            int second = Integer.valueOf(s.substring(i - 2, i));
+            if (first >= 1 && first <= 9) {
+               dp[i] += dp[i-1];  
+            }
+            if (second >= 10 && second <= 26) {
+                dp[i] += dp[i-2];
+            }
+        }
+        return dp[n];
+    }    
 }
