a55ee251472ef5f23d4bf68697a61f78d18f054f
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-05-16 14:58:56 -0700

    .

--- All-Algos/Practice5/DynamicProgramming/DynamicProgramming1.java
+++ All-Algos/Practice5/DynamicProgramming/DynamicProgramming1.java
@@ -83,5 +83,31 @@ public class DynamicProgramming1 {
             }
         }
         return false;
-    }   
+    }
+
+
+    //MAXIMAL SQUARE
+    //[0, 0, 1, 1, 1]
+    //[0, 1, 1, 1, 0] = 4 else == 1;
+    public int maximalSquare(char[][] matrix) {
+        //Possible Brute Force Approach:
+        //loop through array
+        //if (matrix[i[i]] && matrix[i[i++]]) == 1 && (matrix[i++[i]] && matrix[i++[i++]]) == 1
+        //return 4
+        //else return 1
+        
+        //Correct Approach
+        int rows = matrix.length, cols = rows > 0 ? matrix[0].length : 0;
+        int[][] dp = new int[rows + 1][cols + 1];
+        int maxsqlen = 0;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                if (matrix[i-1][j-1] == '1'){
+                    dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
+                    maxsqlen = Math.max(maxsqlen, dp[i][j]);
+                }
+            }
+        }
+        return maxsqlen * maxsqlen;        
+    }    
 }
