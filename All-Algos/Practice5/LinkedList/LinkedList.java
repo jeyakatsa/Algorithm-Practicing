60fa0967acc90a77b8f276e3215b393f308ad087
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-05-24 18:52:55 -0700

    No Easy Way Out

--- All-Algos/Practice5/LinkedList/LinkedList.java
+++ All-Algos/Practice5/LinkedList/LinkedList.java
@@ -115,7 +115,37 @@ public class LinkedList {
             curr.next = new ListNode(carry);
         }
         return dummyHead.next;
-    }  
+    }
+    
+    
+    //ADD TWO NUMBERS II
+    public ListNode addTwoNumbersTwo(ListNode l1, ListNode l2) {
+        Stack<Integer> s1 = new Stack<Integer>();
+        Stack<Integer> s2 = new Stack<Integer>();
+        
+        while(l1 != null) {
+            s1.push(l1.val);
+            l1 = l1.next;
+        };
+        while(l2 != null) {
+            s2.push(l2.val);
+            l2 = l2.next;
+        }
+        
+        int sum = 0;
+        ListNode list = new ListNode(0);
+        while (!s1.empty() || !s2.empty()) {
+            if (!s1.empty()) sum += s1.pop();
+            if (!s2.empty()) sum += s2.pop();
+            list.val = sum % 10;
+            ListNode head = new ListNode(sum / 10);
+            head.next = list;
+            list = head;
+            sum /= 10;
+        }
+        
+        return list.val == 0 ? list.next : list;
+    }        
     
     
     //MERGE TWO SORTED LISTS
