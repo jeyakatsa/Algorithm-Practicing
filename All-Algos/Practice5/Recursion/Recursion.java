5b6ef319a2fb2f6fb271a5c3cedf8e6dae9337a9
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-05-25 19:40:16 -0700

    A Crazy man that loves pain, suffering and challenges...

--- All-Algos/Practice5/Recursion/Recursion.java
+++ All-Algos/Practice5/Recursion/Recursion.java
@@ -32,12 +32,54 @@ public class Recursion {
         return Integer.bitCount(K - 1) & 1;
     }
     // think of the problem like this
-    /*        0
-          /       \
-         0          1
-       /   \      /    \
-      0     1    1      0
-     / \     / \   / \   / \
-    0  1   1   0  1  0  0  1
-    */      
+    //n = row, k = column
+    /*         0
+          /        \
+         0           1
+       /   \       /   \
+      0     1     1     0
+     / \   / \   / \   / \
+    0  1  1   0  1  0  0  1
+    */ 
+    
+
+    
+    //PARTITION TO K EQUAL SUM SUBSETS
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        int sum=0;
+        for(int num:nums) sum+=num;
+        
+        if(sum%k!=0) return false;
+        int target=sum/k;
+        
+        Arrays.sort(nums);
+        if(target<nums[nums.length-1]) return false; 
+        boolean[] used=new boolean[nums.length];
+        
+        return partition(nums,used,0,0,0,target,k);
+    }
+    
+    private boolean partition(int[] nums,boolean[] used,int idx,int currentSum,int currentSet,int targetSum,int targetSet){
+        if(currentSet==targetSet) return true;
+        
+        for(int i=idx;i<nums.length;i++){
+		    /*
+			   2 2 2 3
+			   F T 
+			*/
+            if(used[i] || (i<nums.length-1 && nums[i]==nums[i+1] && used[i+1])) continue;
+            
+            int sum=currentSum+nums[i];
+            if(sum>targetSum) break;
+            
+             used[i]=true;
+            if(sum==targetSum && partition(nums,used,0,0,currentSet+1,targetSum,targetSet)){
+                return true;
+            }else if(partition(nums,used,i+1,sum,currentSet,targetSum,targetSet)){
+                return true;
+            }
+            used[i]=false;
+        }
+        return false;
+    }    
 }
