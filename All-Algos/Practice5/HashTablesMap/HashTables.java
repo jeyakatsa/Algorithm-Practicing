2c0dc5c414a3c1707d8e33a2616b54ebad28fc50
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-05-27 19:48:26 -0700

    ..............

--- All-Algos/Practice5/HashTablesMap/HashTables.java
+++ All-Algos/Practice5/HashTablesMap/HashTables.java
@@ -43,6 +43,20 @@ public class HashTables {
             map.put(nums[i], i);
         }
         throw new IllegalArgumentException("No two sum solution");
-    }      
+    }   
+    
+    //LONGEST SUBSTRING WITHOUT REPEATING CHARACTERS
+    public int lengthOfLongestSubstring(String s) {
+        int ret = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0, start = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) 
+                start = Math.max(map.get(c)+1, start);
+            ret = Math.max(ret, i-start+1); 
+            map.put(c, i);
+        }
+        return ret;
+    }    
 
 }
