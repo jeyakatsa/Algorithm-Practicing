b925e313562a40dffcbd01233732234b1594361c
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-05-28 18:49:25 -0700

    Work

--- All-Algos/Practice5/Arrays/Arrays2.java
+++ All-Algos/Practice5/Arrays/Arrays2.java
@@ -30,4 +30,23 @@ public class Arrays2 {
         return (int) ((maxWidth * maxHeight) % (1e9 + 7));
     }
 
+
+
+    //MOVE ZEROES TO END OF ARRAY
+    public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length == 0) return;        
+
+        int snowBallSize = 0; 
+        for (int i=0;i<nums.length;i++){
+	        if (nums[i]==0){
+                snowBallSize++; 
+            }
+            else if (snowBallSize > 0) {
+	            int t = nums[i];
+	            nums[i]=0;
+	            nums[i-snowBallSize]=t;
+            }
+        }
+    }
+
 }
