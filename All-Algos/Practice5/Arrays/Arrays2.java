327fc00746a16874d6c85eae337381f2f2824060
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-05-28 21:49:08 -0700

    .

--- All-Algos/Practice5/Arrays/Arrays2.java
+++ All-Algos/Practice5/Arrays/Arrays2.java
@@ -77,7 +77,7 @@ public class Arrays2 {
 
 
 
-    
+
     //HIGH FIVE (Average of the top 5 grades of each student)
     private int K;
 
@@ -105,7 +105,39 @@ public class Arrays2 {
         }
         int[][] solutionArray = new int[solution.size()][];
         return solution.toArray(solutionArray);
-    } 
+    }
+    
+    
+    //NEXT PERMUTATION
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    private void reverse(int[] nums, int start) {
+        int i = start, j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }    
 
 
 
