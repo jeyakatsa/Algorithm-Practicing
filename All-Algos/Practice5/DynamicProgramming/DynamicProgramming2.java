4db440fe8ca444ee88590d0c184e2034eeefac75
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-05-29 20:50:08 -0700

    Work Work Work

--- All-Algos/Practice5/DynamicProgramming/DynamicProgramming2.java
+++ All-Algos/Practice5/DynamicProgramming/DynamicProgramming2.java
@@ -30,6 +30,7 @@ public class DynamicProgramming2 {
         }
         return pLeft == leftBound;
     }
+
     
     //DELETE AND EARN (I don't understand this problem...)
     public int deleteAndEarn(int[] nums) {
@@ -49,5 +50,25 @@ public class DynamicProgramming2 {
             prev = k;
         }
         return Math.max(avoid, using);
-    }    
+    }   
+    
+    
+    //MAXIMUM PRODUCT SUBARRAY O(n^2) Space Time
+    public int maxProduct(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int result = nums[0];
+
+        for (int i = 0; i < nums.length; i++) {
+            int accu = 1;
+            for (int j = i; j < nums.length; j++) {
+                accu *= nums[j];
+                result = Math.max(result, accu);
+            }
+        }
+
+        return result;
+    }
+
+
 }
