19a9569f5d1e15fc4fffc3e0d0b6fb7d3941ccec
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-05-29 18:44:50 -0700

    .

--- All-Algos/Practice5/DynamicProgramming/DynamicProgramming2.java
+++ All-Algos/Practice5/DynamicProgramming/DynamicProgramming2.java
@@ -1,4 +1,7 @@
 public class DynamicProgramming2 {
+
+
+    //CLIMBING STAIRS
     public int climbStairs(int n) {
         return climb_Stairs(0, n);
     }
@@ -11,4 +14,20 @@ public class DynamicProgramming2 {
         }
         return climb_Stairs(i + 1, n) + climb_Stairs(i + 2, n);
     }
+
+
+    //IS SUBSEQUENCE OF STRINGS ("s = abc" - "t = abhhdc")
+    public boolean isSubsequence(String s, String t) {
+        Integer leftBound = s.length(), rightBound = t.length();
+        Integer pLeft = 0, pRight = 0;
+
+        while (pLeft < leftBound && pRight < rightBound) {
+            // move both pointers or just the right pointer
+            if (s.charAt(pLeft) == t.charAt(pRight)) {
+                pLeft += 1;
+            }
+            pRight += 1;
+        }
+        return pLeft == leftBound;
+    }    
 }
