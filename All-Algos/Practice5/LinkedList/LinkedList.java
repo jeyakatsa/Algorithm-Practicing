e8cd208c0650a97e717b5d82e8f2f8adf1de2fdc
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-06-01 18:48:02 -0700

    The Process....

--- All-Algos/Practice5/LinkedList/LinkedList.java
+++ All-Algos/Practice5/LinkedList/LinkedList.java
@@ -250,7 +250,7 @@ public class LinkedList {
         return sentinel.next;
     }   
 
-    
+
 
     //REORDER LIST
     public void reorderList(ListNode head) {
@@ -288,7 +288,25 @@ public class LinkedList {
           second.next = first;
           second = tmp;
         }
-      }     
+    }  
+    
+    
+
+
+
+    //INTERSECTION OF TWO LISTS
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        while (headA != null) {
+            ListNode pB = headB;
+            while (pB != null) {
+                if (headA == pB) return headA;
+                pB = pB.next;
+            }
+            headA = headA.next;
+        }
+        return null;
+    }
+
 
 
     
