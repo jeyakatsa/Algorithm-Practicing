6dd43cd52dbffd0e86cf5b95edeb0db477e5fe6a
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-06-03 04:47:48 -0700

    Work

--- All-Algos/Practice5/LinkedList/LinkedList.java
+++ All-Algos/Practice5/LinkedList/LinkedList.java
@@ -325,7 +325,26 @@ public class LinkedList {
         }
         second.next = second.next.next;
         return dummy.next;
-    }   
+    }
+
+
+    //CHECK LINKED LIST CYCLE
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
 
 
 
