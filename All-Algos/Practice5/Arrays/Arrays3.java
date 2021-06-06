1d3e18bbc103619b6256d8cef8284354f4749e92
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-06-06 16:35:43 -0700

    .

--- All-Algos/Practice5/Arrays/Arrays3.java
+++ All-Algos/Practice5/Arrays/Arrays3.java
@@ -10,4 +10,18 @@ public class Arrays3 {
                 maxarea = Math.max(maxarea, Math.min(height[i], height[j]) * (j - i));
         return maxarea;
     }
+
+    //ROTATE IMAGE
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < (n + 1) / 2; i ++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i];
+                matrix[j][n - 1 - i] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+    }  
 }
