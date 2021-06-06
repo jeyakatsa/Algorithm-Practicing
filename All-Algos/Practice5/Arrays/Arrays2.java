6e585950a0f8d16a00b7f4af3b7eafdc050fbd7f
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-06-05 21:50:19 -0700

    .

--- All-Algos/Practice5/Arrays/Arrays2.java
+++ All-Algos/Practice5/Arrays/Arrays2.java
@@ -203,39 +203,27 @@ public class Arrays2 {
     }
 
 
-
-    //TOP K FREQUENT WORDS [O N log N]
-    public List<String> topKFrequent(String[] words, int k) {
-        // Hande null data and edge cases
-        if (words == null | words.length == 0) return new ArrayList<>();
-        
-        // Use a map to keep track of counts
-        Map<String, Integer> map = new HashMap<>();
-        // Use a list to keep track of words, to then sort them
-        List<String> list = new ArrayList<>();
-        
-        for (String word : words) {
-            // Count the # of times a words appears
-            map.put(word, map.getOrDefault(word, 0) + 1);
-            // Load only unique words to list
-            if (!list.contains(word)) list.add(word);
+    //SEARCH IN SORTED ARRAY
+    public int search(int[] nums, int target) {
+        int start = 0, end = nums.length - 1;
+        while (start <= end) {
+          int mid = start + (end - start) / 2;
+          if (nums[mid] == target) return mid;
+          else if (nums[mid] >= nums[start]) {
+            if (target >= nums[start] && target < nums[mid]) end = mid - 1;
+            else start = mid + 1;
+          }
+          else {
+            if (target <= nums[end] && target > nums[mid]) start = mid + 1;
+            else end = mid - 1;
+          }
         }
-        
-        Collections.sort(list, (String a, String b) -> {
-                int aCount = map.get(a);
-                int bCount = map.get(b);
-            
-                // If the counts are equal, then use String.compareTo to lexigraphically compare the strings
-                if (aCount == bCount) {
-                    return a.compareTo(b);
-                } else {  // Else sort by greatest count
-                    return bCount - aCount;
-                }
-            });
-        
-        // Return a list with only up to k elements
-        return list.subList(0, k);
-    }
+        return -1;
+      }
+
+
+
+    
 
 
 
