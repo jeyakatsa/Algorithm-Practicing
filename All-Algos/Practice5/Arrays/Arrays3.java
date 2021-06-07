bdbc6987f784e9194a7ce98dc81630831597e86a
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-06-06 17:37:21 -0700

    .

--- All-Algos/Practice5/Arrays/Arrays3.java
+++ All-Algos/Practice5/Arrays/Arrays3.java
@@ -11,6 +11,9 @@ public class Arrays3 {
         return maxarea;
     }
 
+
+
+
     //ROTATE IMAGE
     public void rotate(int[][] matrix) {
         int n = matrix.length;
@@ -23,5 +26,24 @@ public class Arrays3 {
                 matrix[i][j] = temp;
             }
         }
-    }  
+    } 
+    
+    
+    //Pairs of Songs With Total Durations Divisible by 60
+    public int numPairsDivisibleBy60(int[] time) {
+        int count = 0, n = time.length;
+        for (int i = 0; i < n; i++) {
+            // j starts with i+1 so that i is always to the left of j
+            // to avoid repetitive counting
+            for (int j = i + 1; j < n; j++) {
+                if ((time[i] + time[j]) % 60 == 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+
+
 }
