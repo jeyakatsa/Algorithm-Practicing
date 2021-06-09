240166651f73a08133ac0eee8dd73891607365a3
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-06-08 20:49:58 -0700

    .

--- All-Algos/Practice5/DynamicProgramming/DynamicProgramming2.java
+++ All-Algos/Practice5/DynamicProgramming/DynamicProgramming2.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class DynamicProgramming2 {
 
 
@@ -115,7 +117,28 @@ public class DynamicProgramming2 {
             return true;    
         }
         return false;
-    }      
+    }   
+    
+    
+    //MAXUMUM LENGTH OF REPEATED SUBARRAY'
+    public int findLength(int[] A, int[] B) {
+        int ans = 0;
+        Map<Integer, ArrayList<Integer>> Bstarts = new HashMap<>();
+        for (int j = 0; j < B.length; j++) {
+            Bstarts.computeIfAbsent(B[j], x -> new ArrayList<>()).add(j);
+        }
+
+        for (int i = 0; i < A.length; i++) if (Bstarts.containsKey(A[i])) {
+            for (int j: Bstarts.get(A[i])) {
+                int k = 0;
+                while (i+k < A.length && j+k < B.length && A[i+k] == B[j+k]) {
+                    k++;
+                }
+                ans = Math.max(ans, k);
+            }
+        }
+        return ans;
+    }  
 
 
 
