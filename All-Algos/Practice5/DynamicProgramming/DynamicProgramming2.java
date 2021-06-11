2fcfe51408d45b1aada6c1f788b33720d13cd70e
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-06-10 18:45:17 -0700

    .

--- All-Algos/Practice5/DynamicProgramming/DynamicProgramming2.java
+++ All-Algos/Practice5/DynamicProgramming/DynamicProgramming2.java
@@ -188,6 +188,23 @@ public class DynamicProgramming2 {
     }    
 
 
+    //CONTINUOUS SUBARRAY SUM
+    public boolean checkSubarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>(){{put(0,-1);}};;
+        int runningSum = 0;
+        for (int i=0;i<nums.length;i++) {
+            runningSum += nums[i];
+            if (k != 0) runningSum %= k; 
+            Integer prev = map.get(runningSum);
+            if (prev != null) {
+                if (i - prev > 1) return true;
+            }
+            else map.put(runningSum, i);
+        }
+        return false;
+    }    
+
+
 
 
 
