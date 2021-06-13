0d052c7662160e59df0381aa99ed273688d83b21
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-06-12 17:40:55 -0700

    .

--- All-Algos/Practice5/Trees/Trees1.java
+++ All-Algos/Practice5/Trees/Trees1.java
@@ -164,7 +164,33 @@ public class Trees1 {
         }
         
         return dp[0][arr.length - 1];
-    }    
+    }  
+
+    
+    
+    //BINARY TREE LEVEL ORDER TRAVERSAL
+    public List<List<Integer>> levelOrder(Node root) {
+		List<List<Integer>> res = new ArrayList<>();
+		if (root == null)
+			return res;
+		Queue<Node> q = new LinkedList<>();
+		q.add(root);
+		while(!q.isEmpty()) {
+			int levelSize = q.size();
+			List<Integer> currLevel = new ArrayList<>();
+			for(int i = 0; i < levelSize; i++) {
+				Node currNode = q.poll();
+				currLevel.add(currNode.val);
+				if (currNode.left != null)
+					q.add(currNode.left);
+				if (currNode.right != null)
+					q.add(currNode.right);
+			}
+			res.add(currLevel);
+		}
+		return res;
+	} 
+
     
 
 
