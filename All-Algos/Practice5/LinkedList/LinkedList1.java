1948c743bce2e98170c8a89d83439e16571aaf6c
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-06-13 16:50:06 -0700

    C:/Program Files/Git/

--- All-Algos/Practice5/LinkedList/LinkedList1.java
+++ All-Algos/Practice5/LinkedList/LinkedList1.java
@@ -17,11 +17,20 @@ public class LinkedList1 {
     public static class Node {
         int val;
         Node next;
+        Node prev;
+        Node child;
         public int label;
         public Object random;
         Node() {}
         Node(int val) { this.val = val; }
         Node(int val, Node next) { this.val = val; this.next = next; }
+        Node(int val, Node next, Node prev) { this.prev = prev; this.val = val; this.next = next; }
+        Node(int _val,Node _prev,Node _next,Node _child) {
+            val = _val;
+            prev = _prev;
+            next = _next;
+            child = _child;
+        }        
     }
 
 
@@ -61,7 +70,41 @@ public class LinkedList1 {
         // Case 3).
         prev.next = new Node(insertVal, curr);
         return head;
-      }    
+    }  
+    
+    
+
+
+    //Flatten a Multilevel Doubly Linked List
+    public Node flatten(Node head) {
+        if (head == null) return head;
+        // pseudo head to ensure the `prev` pointer is never none
+        Node pseudoHead = new Node(0, null, head, null);
+
+        flattenDFS(pseudoHead, head);
+
+        // detach the pseudo head from the real head
+        pseudoHead.next.prev = null;
+        return pseudoHead.next;
+    }
+    /* return the tail of the flatten list */
+    public Node flattenDFS(Node prev, Node curr) {
+        if (curr == null) return prev;
+        curr.prev = prev;
+        prev.next = curr;
+
+        // the curr.next would be tempered in the recursive function
+        Node tempNext = curr.next;
+
+        Node tail = flattenDFS(curr, curr.child);
+        curr.child = null;
+
+        return flattenDFS(tail, tempNext);
+    }   
+
+
+
+
     
 
     
