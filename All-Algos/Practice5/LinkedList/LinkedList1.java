26b552312d01a36b74496849cee9af9e0fea6e6f
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-06-13 18:53:09 -0700

    .

--- All-Algos/Practice5/LinkedList/LinkedList1.java
+++ All-Algos/Practice5/LinkedList/LinkedList1.java
@@ -100,7 +100,52 @@ public class LinkedList1 {
         curr.child = null;
 
         return flattenDFS(tail, tempNext);
-    }   
+    } 
+
+
+
+    //Partition List
+    public Node partition(Node head, int x) {
+        //if head.val >= x
+        //create new head node. and transfer all said nodes to new head
+
+        // before and after are the two pointers used to create the two list
+        // before_head and after_head are used to save the heads of the two lists.
+        // All of these are initialized with the dummy nodes created.
+        Node before_head = new Node(0);
+        Node before = before_head;
+        Node after_head = new Node(0);
+        Node after = after_head;
+
+        while (head != null) {
+
+            // If the original list node is lesser than the given x,
+            // assign it to the before list.
+            if (head.val < x) {
+                before.next = head;
+                before = before.next;
+            } else {
+                // If the original list node is greater or equal to the given x,
+                // assign it to the after list.
+                after.next = head;
+                after = after.next;
+            }
+
+            // move ahead in the original list
+            head = head.next;
+        }
+
+        // Last node of "after" list would also be ending node of the reformed list
+        after.next = null;
+
+        // Once all the nodes are correctly assigned to the two lists,
+        // combine them to form a single list which would be returned.
+        before.next = after_head.next;
+
+        return before_head.next;
+    }    
+
+    
 
 
 
