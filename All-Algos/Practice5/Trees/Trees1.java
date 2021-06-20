66c03a2e362ff7e56694a21aab546f93deaa4509
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-06-20 15:24:00 -0700

    .

--- All-Algos/Practice5/Trees/Trees1.java
+++ All-Algos/Practice5/Trees/Trees1.java
@@ -126,6 +126,7 @@ public class Trees1 {
         return res;
     } 
 
+    
 
     //LONELIEST COMMON ANCESTOR OF A BINARY TREE
     public Node lowestCommonAncestor(Node p, Node q) {
@@ -145,12 +146,9 @@ public class Trees1 {
         for (int i = l; i <= h; i++) tmp = Math.max(tmp, arr[i]);
         return tmp;
     }
-    
     public int mctFromLeafValues(int[] arr) {
-        if (arr == null || arr.length <= 1) return 0;
-        
-        int[][] dp = new int[arr.length][arr.length];
-        
+        if (arr == null || arr.length <= 1) return 0;     
+        int[][] dp = new int[arr.length][arr.length];   
         for (int j = 0; j < arr.length; j++){
             for (int i = j; i >= 0; i--){
                 for (int k = i; k < j; k++){
@@ -161,8 +159,7 @@ public class Trees1 {
                         dp[i][j] = Math.min(dp[i][j], tmp);
                 }
             }
-        }
-        
+        }   
         return dp[0][arr.length - 1];
     }  
 
--- All-Algos/Practice5/Trees/Trees3.java
+++ All-Algos/Practice5/Trees/Trees3.java
@@ -35,5 +35,28 @@ public class Trees3 {
     
     
 
-    
+    //Diameter of Binary Tree
+    private int diameter;
+    public int diameterOfBinaryTree(Node root) {
+        diameter = 0;
+        longestPath(root);
+        return diameter;
+    }
+    private int longestPath(Node node){
+        if(node == null) return 0;
+        // recursively find the longest path in
+        // both left child and right child
+        int leftPath = longestPath(node.left);
+        int rightPath = longestPath(node.right);
+
+        // update the diameter if left_path plus right_path is larger
+        diameter = Math.max(diameter, leftPath + rightPath);
+
+        // return the longest one between left_path and right_path;
+        // remember to add 1 for the path connecting the node and its parent
+        return Math.max(leftPath, rightPath) + 1;
+    }   
+
+
+
 }
