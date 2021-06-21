f9b5b5b059586ad4d3ad0fa74b87f4a83e36462b
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-06-20 17:19:57 -0700

    .

--- All-Algos/Practice5/Trees/Trees3.java
+++ All-Algos/Practice5/Trees/Trees3.java
@@ -75,6 +75,20 @@ public class Trees3 {
             return Math.abs(o1 - target) < Math.abs(o2 - target) ? -1 : 1;
             }
         });
+    } 
+
+
+
+    //Kill Process
+    public List <Integer> killProcess(List <Integer> pid, List <Integer> ppid, int kill) {
+        List <Integer> l = new ArrayList<>();
+        if (kill == 0)
+            return l;
+        l.add(kill);
+        for (int i = 0; i < ppid.size(); i++)
+            if (ppid.get(i) == kill)
+                l.addAll(killProcess(pid, ppid, pid.get(i)));
+        return l;
     }   
 
 
