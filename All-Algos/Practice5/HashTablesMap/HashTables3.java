db29a4933f4f7d1b021c819cf23ee956713d1d45
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-06-26 13:45:16 -0700

    .

--- All-Algos/Practice5/HashTablesMap/HashTables3.java
+++ All-Algos/Practice5/HashTablesMap/HashTables3.java
@@ -3,11 +3,11 @@ import java.util.*;
 public class HashTables3 {
 
 
+    //Tweet Counts Per Frequency
     Map<String, List<Integer>> map;
     public HashTables3() {
         map = new HashMap<>(); 
     }
-    
     public void recordTweet(String tweetName, int time) {
         if(!map.containsKey(tweetName)){
             List<Integer> temp = new ArrayList<>();
@@ -16,25 +16,20 @@ public class HashTables3 {
         }else{
             map.get(tweetName).add(time);
         }
-        
     }
-    
     public List<Integer> getTweetCountsPerFrequency(String freq, String tweetName, int startTime, int endTime) {
-        
         int interval = 60;
         if(freq.equals("hour"))
             interval = interval * 60;
         if(freq.equals("day"))
             interval = interval * 60 * 24;
         List<Integer> res = new ArrayList<>();
-        
         // get the number of possible intervals, 
         // if startTime = 30 and endTime = 150 with minute as freq
         // (150 - 30) / 60 = 2, this means there will be 3 intervals
         // [30, 90); [90, 150); [150, 150)
         for(int i = 0; i <= (endTime - startTime) / interval; i++)
             res.add(0);
-        
         List<Integer> times = map.get(tweetName);
         for(int time : times){
             if(startTime <= time && time <= endTime){
