5d1c0a4cb67ed813125f6c4d50596eb6e08e6169
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-06-26 21:55:10 -0700

    .

--- All-Algos/Practice5/Arrays/Arrays5.java
+++ All-Algos/Practice5/Arrays/Arrays5.java
@@ -92,6 +92,29 @@ public class Arrays5 {
         }
         return true;
     } 
+
+
+    //Design Hit Counter
+    private Queue<Integer> hits; 
+    /** Initialize your data structure here. */
+    public Arrays5() {
+        this.hits = new LinkedList<Integer>();
+    }
+    /** Record a hit.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    public void hit(int timestamp) {
+        this.hits.add(timestamp);
+    }
+    /** Return the number of hits in the past 5 minutes.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    public int getHits(int timestamp) {
+        while (!this.hits.isEmpty()) {
+            int diff = timestamp - this.hits.peek();
+            if (diff >= 300) this.hits.remove();
+            else break;
+        }
+        return this.hits.size();
+    }    
     
     
 }
