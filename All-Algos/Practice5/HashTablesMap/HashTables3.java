8ef78bcce47f250b4a51f515c2f975742124f6f3
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-06-27 16:40:33 -0700

    .

--- All-Algos/Practice5/HashTablesMap/HashTables3.java
+++ All-Algos/Practice5/HashTablesMap/HashTables3.java
@@ -137,7 +137,7 @@ public class HashTables3 {
         return sb.toString();
     }
 
-    
+
 
 
     //Encode and Decode TinyURL
@@ -148,7 +148,34 @@ public class HashTables3 {
     }
     public String decode(String shortUrl) {
         return mapp.get(Integer.parseInt(shortUrl.replace("http://tinyurl.com/", "")));
+    }  
+    
+    
+
+    //Line Reflection
+    public boolean isReflected(int[][] points) {
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        HashSet<String> set = new HashSet<>();
+        for(int[] p:points){
+            max = Math.max(max,p[0]);
+            min = Math.min(min,p[0]);
+            String str = p[0] + "a" + p[1];
+            set.add(str);
+        }
+        int sum = max+min;
+        for(int[] p:points){
+            //int[] arr = {sum-p[0],p[1]};
+            String str = (sum-p[0]) + "a" + p[1];
+            if( !set.contains(str))
+                return false;
+            
+        }
+        return true;
     }    
+
+
+
 
 
 }
