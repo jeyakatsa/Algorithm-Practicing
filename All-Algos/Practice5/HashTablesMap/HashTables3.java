c69e6e9b4426d34ea447a9c86b43732320d327bc
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-06-27 14:26:34 -0700

    .

--- All-Algos/Practice5/HashTablesMap/HashTables3.java
+++ All-Algos/Practice5/HashTablesMap/HashTables3.java
@@ -114,4 +114,23 @@ public class HashTables3 {
     }    
 
 
+
+
+    //Integer to Roman
+    private static final int[] valuess = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};    
+    private static final String[] symbols = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+    public String intToRoman(int num) {
+        StringBuilder sb = new StringBuilder();
+        // Loop through each symbol, stopping if num becomes 0.
+        for (int i = 0; i < valuess.length && num > 0; i++) {
+            // Repeat while the current symbol still fits into num.
+            while (valuess[i] <= num) {
+                num -= valuess[i];
+                sb.append(symbols[i]);
+            }
+        }
+        return sb.toString();
+    }
+
+
 }
