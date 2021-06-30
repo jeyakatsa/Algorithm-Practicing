d783e4b6f907cd0891b5734a0b26c11254ae55f6
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-06-29 18:32:22 -0700

    .

--- All-Algos/Practice5/DynamicProgramming/DynamicProgramming4.java
+++ All-Algos/Practice5/DynamicProgramming/DynamicProgramming4.java
@@ -43,6 +43,20 @@ public class DynamicProgramming4 {
         }
         res += sum_cost - max_cost;
         return res;  
-    }    
+    }
+
+
+    //Coin Change 2
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+    
+        for (int coin : coins) {
+          for (int x = coin; x < amount + 1; ++x) {
+            dp[x] += dp[x - coin];
+          }
+        }
+        return dp[amount];
+      }    
     
 }
