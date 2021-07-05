554f8b845625444afe493f7a67a186f73b95b462
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-07-04 19:50:33 -0700

    .

--- All-Algos/Practice5/Arrays/Arrays6.java
+++ All-Algos/Practice5/Arrays/Arrays6.java
@@ -42,7 +42,6 @@ public class Arrays6 {
         Collections.swap(nums, first, i);
         }
     }
-
     public List<List<Integer>> permute(int[] nums) {
         // init output list
         List<List<Integer>> output = new LinkedList<>();
@@ -55,7 +54,32 @@ public class Arrays6 {
         int n = nums.length;
         backtrack(n, nums_lst, output, 0);
         return output;
-    }    
+    }   
+    
+    
+    //Interval List Intersections...
+    public int[][] intervalIntersection(int[][] A, int[][] B) {
+        List<int[]> ans = new ArrayList<>();
+        int i = 0, j = 0;
+    
+        while (i < A.length && j < B.length) {
+          // Let's check if A[i] intersects B[j].
+          // lo - the startpoint of the intersection
+          // hi - the endpoint of the intersection
+          int lo = Math.max(A[i][0], B[j][0]);
+          int hi = Math.min(A[i][1], B[j][1]);
+          if (lo <= hi)
+            ans.add(new int[]{lo, hi});
+    
+          // Remove the interval with the smallest endpoint
+          if (A[i][1] < B[j][1])
+            i++;
+          else
+            j++;
+        }
+    
+        return ans.toArray(new int[ans.size()][]);
+      }    
 
 
     
