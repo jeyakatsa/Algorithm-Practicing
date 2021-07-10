562b16efcb39daf8da4630d8c714bdccb68d93e2
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-07-09 18:43:11 -0700

    .

--- All-Algos/Practice5/Arrays/Arrays6.java
+++ All-Algos/Practice5/Arrays/Arrays6.java
@@ -149,6 +149,43 @@ public class Arrays6 {
   }    
 
 
+  //Design Leaderboard
+  class Leaderboard {
+
+    private HashMap<Integer, Integer> scores;
+    
+    public Leaderboard() {
+        this.scores = new HashMap<Integer, Integer>();
+    }
+    
+    public void addScore(int playerId, int score) {
+        
+        if (!this.scores.containsKey(playerId)) {
+            this.scores.put(playerId, 0);
+        }
+        
+        this.scores.put(playerId, this.scores.get(playerId) + score);
+    }
+    
+    public int top(int K) {
+        
+        List<Integer> values = new ArrayList<Integer>(this.scores.values());
+        Collections.sort(values, Collections.reverseOrder());
+        
+        int total = 0;
+        for (int i = 0; i < K; i++) {
+            total += values.get(i);            
+        }
+        
+        return total;
+    }
+    
+    public void reset(int playerId) {
+        this.scores.put(playerId, 0);
+    }
+  }
+
+
 
 
 
