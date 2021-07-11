5f93cdde16969094b7111ee3f1f19462218cb330
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-07-10 17:50:37 -0700

    .

--- All-Algos/Practice5/HashTablesMap/HashTables5.java
+++ All-Algos/Practice5/HashTablesMap/HashTables5.java
@@ -122,6 +122,26 @@ public class HashTables5 {
         }
     }
     return ans;
-}  
+  } 
+  
+  
+  //Happy Number
+  private int getNext(int n) {
+    int totalSum = 0;
+    while (n > 0) {
+        int d = n % 10;
+        n = n / 10;
+        totalSum += d * d;
+    }
+    return totalSum;
+  }
+  public boolean isHappy(int n) {
+    Set<Integer> seen = new HashSet<>();
+    while (n != 1 && !seen.contains(n)) {
+        seen.add(n);
+        n = getNext(n);
+    }
+    return n == 1;
+  }  
     
 }
