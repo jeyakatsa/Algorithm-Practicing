349d639ea6797ccadd5fd022be66564e75c9af5a
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-07-13 16:59:38 -0700

    .

--- All-Algos/Practice5/Arrays/Arrays7.java
+++ All-Algos/Practice5/Arrays/Arrays7.java
@@ -3,28 +3,52 @@ import java.util.*;
 public class Arrays7 {
 
     //Asteroid Collision
-    public int[] asteroidCollision(int[] asteroids) {
-        Stack<Integer> stack = new Stack();
-        for (int ast: asteroids) {
-            collision: {
-                while (!stack.isEmpty() && ast < 0 && 0 < stack.peek()) {
-                    if (stack.peek() < -ast) {
-                        stack.pop();
-                        continue;
-                    } else if (stack.peek() == -ast) {
-                        stack.pop();
-                    }
-                    break collision;
+    public int[] asteroidCollision(int[] a) {
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < a.length; i++) {
+            if (stack.isEmpty() || a[i] > 0) {
+                stack.push(a[i]);
+                continue;
+            }
+            
+            while (true) {
+                int prev = stack.peek();
+                if (prev < 0) {
+                    stack.push(a[i]);
+                    break;
                 }
-                stack.push(ast);
+                if (prev == -a[i]) {
+                    stack.pop();
+                    break;
+                }
+                if (prev > -a[i]) {
+                    break;
+                }
+                stack.pop();
+                if (stack.isEmpty()) {
+                    stack.push(a[i]);
+                    break;
+                }
             }
         }
-
-        int[] ans = new int[stack.size()];
-        for (int t = ans.length - 1; t >= 0; --t) {
-            ans[t] = stack.pop();
+        
+        int[] res = new int[stack.size()];
+        for (int i = stack.size() - 1; i >= 0; i--) {
+            res[i] = stack.pop();
         }
-        return ans;
+        
+        return res;
     }
+
+    //Minimum Difference Between Largest and Smallest Value in Three Moves
+    public int minDifference(int[] A) {
+        int n = A.length, res = Integer.MAX_VALUE;
+        if (n < 5) return 0;
+        Arrays.sort(A);
+        for (int i = 0; i < 4; ++i) {
+            res = Math.min(res, A[n - 4 + i] - A[i]);
+        }
+        return res;
+    }    
     
 }
