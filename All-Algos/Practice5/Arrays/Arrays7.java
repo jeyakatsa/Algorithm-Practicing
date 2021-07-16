3f8ee834faff0a6c072e906984d995675c9c583c
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-07-15 17:56:09 -0700

    .

--- All-Algos/Practice5/Arrays/Arrays7.java
+++ All-Algos/Practice5/Arrays/Arrays7.java
@@ -94,6 +94,23 @@ public class Arrays7 {
         } else {
             return new int[]{r, c};
         }
-    }    
+    }
+
+
+    //Maximum Units on a Truck
+    int maximumUnits(int[][] boxTypes, int truckSize) {
+        PriorityQueue<int[]> queue = new PriorityQueue<>((a, b)->b[1] - a[1]);
+        queue.addAll(Arrays.asList(boxTypes));
+        int unitCount = 0;
+        while (!queue.isEmpty()) {
+            int[] top = queue.poll();
+            int boxCount = Math.min(truckSize, top[0]);
+            unitCount += boxCount * top[1];
+            truckSize -= boxCount;
+            if(truckSize == 0)
+                break;
+        }
+        return unitCount;
+    }        
     
 }
