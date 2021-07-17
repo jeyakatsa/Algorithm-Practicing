7cfefe93b8b7fdf6fdd2ec4f33f6539f59603eb2
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-07-16 17:20:43 -0700

    .

--- All-Algos/Practice5/HashTablesMap/HashTables6.java
+++ All-Algos/Practice5/HashTablesMap/HashTables6.java
@@ -61,8 +61,29 @@ public class HashTables6 {
 
 
 
-    //
-
+    //Add Bold Tag In String
+    public String addBoldTag(String s, String[] dict) {
+        int n = s.length();
+        int[] mark = new int[n+1];
+        for(String d : dict) {
+            int i = -1;
+            while((i = s.indexOf(d, i+1)) >= 0) {
+                mark[i]++;
+                mark[i + d.length()]--;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        int sum = 0;
+        for(int i = 0; i <= n; i++) {
+            int cur = sum + mark[i];
+            if (cur > 0 && sum == 0) sb.append("<b>");
+            if (cur == 0 && sum > 0) sb.append("</b>");
+            if (i == n) break;
+            sb.append(s.charAt(i));
+            sum = cur;
+        }
+        return sb.toString();
+    }
 
     
     
