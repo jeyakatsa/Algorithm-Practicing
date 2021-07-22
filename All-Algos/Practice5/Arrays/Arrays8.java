47b39574b717fc43589fea93a30651865b024f11
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-07-22 16:51:37 -0700

    .

--- All-Algos/Practice5/Arrays/Arrays8.java
+++ All-Algos/Practice5/Arrays/Arrays8.java
@@ -151,7 +151,33 @@ public class Arrays8 {
           }
         }
         return output;
-      }    
+    }    
+
+
+    //Minimum Cost to Connect Sticks
+    public int connectSticks(int[] sticks) {
+        int totalCost = 0;
+ 
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+ 
+        // add all sticks to the min heap.
+        for (int stick : sticks) {
+            pq.add(stick);
+        }
+ 
+        // combine two of the smallest sticks until we are left with just one.
+        while (pq.size() > 1) {
+            int stick1 = pq.remove();
+            int stick2 = pq.remove();
+            
+            int cost = stick1 + stick2;
+            totalCost += cost;
+            
+            pq.add(stick1 + stick2);
+        }
+ 
+        return totalCost;
+    }    
 
 
 
