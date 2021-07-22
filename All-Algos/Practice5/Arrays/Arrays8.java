5ca41c4cb96c7a471bfaa17b09f73dfb5e1d3d3e
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-07-21 18:42:42 -0700

    .

--- All-Algos/Practice5/Arrays/Arrays8.java
+++ All-Algos/Practice5/Arrays/Arrays8.java
@@ -136,6 +136,23 @@ public class Arrays8 {
         return result;
     }  
 
+    //Subsets
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> output = new ArrayList<>();
+        output.add(new ArrayList<Integer>());
+    
+        for (int num : nums) {
+          List<List<Integer>> newSubsets = new ArrayList<>();
+          for (List<Integer> curr : output) {
+            newSubsets.add(new ArrayList<Integer>(curr){{add(num);}});
+          }
+          for (List<Integer> curr : newSubsets) {
+            output.add(curr);
+          }
+        }
+        return output;
+      }    
+
 
 
     
