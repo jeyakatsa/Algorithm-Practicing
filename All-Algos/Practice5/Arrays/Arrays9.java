dfac8679087e90cf01ce2ed4598361316db42de4
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-07-24 20:50:46 -0700

    .

--- All-Algos/Practice5/Arrays/Arrays9.java
+++ All-Algos/Practice5/Arrays/Arrays9.java
@@ -23,6 +23,16 @@ public class Arrays9 {
         Arrays.sort(result);        
         return result;
         
-    }    
+    }  
+
+    //Shuffle the Array
+    public int[] shuffle(int[] nums, int n) {
+        int[] res = new int[2 * n];
+        for (int i = 0, j = n, idx = 0; idx < res.length; i++, j++) {
+            res[idx++] = nums[i];
+            res[idx++] = nums[j];
+        } 
+        return res;
+    }      
     
 }
