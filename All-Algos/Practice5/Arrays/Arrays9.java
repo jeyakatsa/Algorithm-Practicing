82f3f057ca871a57d3f74b88a808a9006c000830
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-07-26 18:11:08 -0700

    .

--- All-Algos/Practice5/Arrays/Arrays9.java
+++ All-Algos/Practice5/Arrays/Arrays9.java
@@ -133,6 +133,32 @@ public class Arrays9 {
     private long sumFromNtoX(long n, long x){
         return (n * (n+1))/2 - (x * (x+1))/2;
     }  
+
+
+    //Sum of Subarray Minimums
+    public int sumSubarrayMins(int[] A) {
+        long res = 0, mod = (int)1e9 + 7;
+        int n = A.length, left[] = new int[n], right[] = new int[n];
+        Stack<int[]> s1 = new Stack<>(), s2 = new Stack<>();
+        for (int i = 0; i < n; ++i) {
+            int count = 1;
+            while (!s1.isEmpty() && s1.peek()[0] > A[i])
+                count += s1.pop()[1];
+            s1.push(new int[] {A[i], count});
+            left[i] = count;
+        }
+        for (int i = n - 1; i >= 0; --i) {
+            int count = 1;
+            while (!s2.isEmpty() && s2.peek()[0] >= A[i])
+                count += s2.pop()[1];
+            s2.push(new int[] {A[i], count});
+            right[i] = count;
+        }
+        for (int i = 0; i < n; ++i)
+            res = (res + (long)A[i] * left[i] * right[i]) % mod;
+        return (int)res;
+    }    
+
   
 
     
