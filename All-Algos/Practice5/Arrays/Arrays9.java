6c0447849351b86878481411b7d8392d578e3535
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-07-30 17:52:17 -0700

    .

--- All-Algos/Practice5/Arrays/Arrays9.java
+++ All-Algos/Practice5/Arrays/Arrays9.java
@@ -169,6 +169,22 @@ public class Arrays9 {
         }
         return result;
         
+    }
+
+    
+    //Shortest Word Distance
+    public int shortestDistance(String[] words, String word1, String word2) {
+        int minDistance = words.length;
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(word1)) {
+                for (int j = 0; j < words.length; j++) {
+                    if (words[j].equals(word2)) {
+                        minDistance = Math.min(minDistance, Math.abs(i - j));
+                    }
+                }
+            }
+        }
+        return minDistance;
     }    
 
   
