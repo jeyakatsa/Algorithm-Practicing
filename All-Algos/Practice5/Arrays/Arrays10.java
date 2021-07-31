4295a3400c617b4380a1e199bd625a30f6712f9d
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-07-31 16:30:39 -0700

    .

--- All-Algos/Practice5/Arrays/Arrays10.java
+++ All-Algos/Practice5/Arrays/Arrays10.java
@@ -64,5 +64,20 @@ public class Arrays10 {
         return res;
     }
 
+
+    //Can Place Flowers
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int i = 0, count = 0;
+        while (i < flowerbed.length) {
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)) {
+                flowerbed[i] = 1;
+                count++;
+            }
+            i++;
+        }
+        return count >= n;
+    }    
+
+
     
 }
