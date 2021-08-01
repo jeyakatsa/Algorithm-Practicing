900196f91f8f99732d9c3cfe1b196216ed81d55b
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-08-01 12:36:07 -0700

    .

--- All-Algos/Practice5/Arrays/Arrays10.java
+++ All-Algos/Practice5/Arrays/Arrays10.java
@@ -76,7 +76,25 @@ public class Arrays10 {
             i++;
         }
         return count >= n;
-    }    
+    } 
+
+
+    //Partition Array for Maximum Sum 
+    public int maxSumAfterPartitioning(int[] arr, int k) {
+        int N=arr.length;
+        if(N==1) return arr[0];
+        int[] dp = new int[N+1];
+        
+        for(int i=1;i<=N;i++){
+            int currMax=0; int maxSumK=0;
+            for(int sub=1;sub<=k && i-sub>=0;sub++){
+                currMax=Math.max(currMax,arr[i-sub]);
+                maxSumK=Math.max(maxSumK,dp[i-sub]+(currMax*sub));
+            }
+            dp[i]=maxSumK;
+        }
+        return dp[N];
+    }      
 
 
     
