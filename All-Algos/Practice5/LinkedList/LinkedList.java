113f3def3f15da30ba754bd6e308cc16f6fef46d
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-08-04 18:57:48 -0700

    .

--- All-Algos/Practice5/LinkedList/LinkedList.java
+++ All-Algos/Practice5/LinkedList/LinkedList.java
@@ -18,7 +18,7 @@ public class LinkedList {
         int val;
         ListNode next;
         public int label;
-        public Object random;
+        public ListNode random;
         ListNode() {}
         ListNode(int val) { this.val = val; }
         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
@@ -66,6 +66,41 @@ public class LinkedList {
             data[key] = -1;
         }
     }
+
+
+    //Copy List with Random Pointer
+    // HashMap which holds old nodes as keys and new nodes as its values.
+    HashMap<ListNode, ListNode> visitedHash = new HashMap<ListNode, ListNode>();
+    public ListNode copyRandomList(ListNode head) {
+
+        if (head == null) {
+        return null;
+        }
+
+        // If we have already processed the current node, then we simply return the cloned version of
+        // it.
+        if (this.visitedHash.containsKey(head)) {
+        return this.visitedHash.get(head);
+        }
+
+        // Create a new node with the value same as old node. (i.e. copy the node)
+        ListNode node = new ListNode(head.val, null);
+
+        // Save this value in the hash map. This is needed since there might be
+        // loops during traversal due to randomness of random pointers and this would help us avoid
+        // them.
+        this.visitedHash.put(head, node);
+
+        // Recursively copy the remaining linked list starting once from the next pointer and then from
+        // the random pointer.
+        // Thus we have two independent recursive calls.
+        // Finally we update the next and random pointers for the new node created.
+        node.next = this.copyRandomList(head.next);
+        node.random = this.copyRandomList(head.random);
+
+        return node;
+    }
+
 
 
    
