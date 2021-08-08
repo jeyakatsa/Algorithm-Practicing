edbe39a46f20d61d7ef012c7c42b1f204121aefb
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-08-07 21:27:32 -0700

    .

--- All-Algos/Practice5/DynamicProgramming/DynamicProgramming1.java
+++ All-Algos/Practice5/DynamicProgramming/DynamicProgramming1.java
@@ -76,6 +76,24 @@ public class DynamicProgramming1 {
             prev = k;
         }
         return Math.max(avoid, using);
+    }  
+
+    //Word Break
+    public boolean wordBreak(String s, List<String> wordDict) {
+        return wordBreakRecur(s, new HashSet<>(wordDict), 0);
+    }
+
+    private boolean wordBreakRecur(String s, Set<String> wordDict, int start) {
+        if (start == s.length()) {
+            return true;
+        }
+        for (int end = start + 1; end <= s.length(); end++) {
+            if (wordDict.contains(s.substring(start, end)) &&
+            wordBreakRecur(s, wordDict, end)) {
+                return true;
+            }
+        }
+        return false;
     }     
     
 }
