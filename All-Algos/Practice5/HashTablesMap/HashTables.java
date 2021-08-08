ab60b0201b0c6a219e5678494d8750e4167a634b
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-08-08 15:50:56 -0700

    .

--- All-Algos/Practice5/HashTablesMap/HashTables.java
+++ All-Algos/Practice5/HashTablesMap/HashTables.java
@@ -29,6 +29,31 @@ public class HashTables {
         // In case there is no solution, we'll just return null
         return null;
     }
+
+    //Longest Substring Without Repeating Characters
+    public int lengthOfLongestSubstring(String s) {
+        int[] chars = new int[128];
+
+        int left = 0;
+        int right = 0;
+
+        int res = 0;
+        while (right < s.length()) {
+            char r = s.charAt(right);
+            chars[r]++;
+
+            while (chars[r] > 1) {
+                char l = s.charAt(left);
+                chars[l]--;
+                left++;
+            }
+
+            res = Math.max(res, right - left + 1);
+
+            right++;
+        }
+        return res;
+    }    
  
 
 
