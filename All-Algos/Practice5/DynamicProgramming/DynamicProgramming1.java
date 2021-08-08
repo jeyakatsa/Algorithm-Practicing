284176afc05534795ec9a2cd23c5bdd5e0895e39
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-08-07 19:43:51 -0700

    .

--- All-Algos/Practice5/DynamicProgramming/DynamicProgramming1.java
+++ All-Algos/Practice5/DynamicProgramming/DynamicProgramming1.java
@@ -41,6 +41,20 @@ public class DynamicProgramming1 {
             R++;
         }
         return R - L - 1;
-    }    
+    } 
+    
+    //Maximym Subarray
+    public int maxSubArray(int[] nums) {
+        int maxSubarray = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            int currentSubarray = 0;
+            for (int j = i; j < nums.length; j++) {
+                currentSubarray += nums[j];
+                maxSubarray = Math.max(maxSubarray, currentSubarray);
+            }
+        }
+        
+        return maxSubarray;
+    }
     
 }
