e910500cddfe6f9c043f54a93ac8894a897c78ff
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-08-08 16:42:22 -0700

    .

--- All-Algos/Practice5/HashTablesMap/HashTables.java
+++ All-Algos/Practice5/HashTablesMap/HashTables.java
@@ -17,17 +17,17 @@ public class HashTables {
     //         }
     //     } return ans;
     // } 
-    public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
+    public int subarraySum(int[] nums, int k) {
+        int count = 0, sum = 0;
+        HashMap < Integer, Integer > map = new HashMap < > ();
+        map.put(0, 1);
         for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            if (map.containsKey(complement)) {
-                return new int[] { map.get(complement), i };
-            }
-            map.put(nums[i], i);
+            sum += nums[i];
+            if (map.containsKey(sum - k))
+                count += map.get(sum - k);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
-        // In case there is no solution, we'll just return null
-        return null;
+        return count;
     }
 
     //Longest Substring Without Repeating Characters
@@ -53,6 +53,35 @@ public class HashTables {
             right++;
         }
         return res;
+    }    
+
+
+    //Verifying Alien Dictionary
+    public boolean isAlienSorted(String[] words, String order) {
+        int[] orderMap = new int[26];
+        for (int i = 0; i < order.length(); i++){
+            orderMap[order.charAt(i) - 'a'] = i;
+        }
+
+        for (int i = 0; i < words.length - 1; i++) {
+
+            for (int j = 0; j < words[i].length(); j++) {
+                // If we do not find a mismatch letter between words[i] and words[i + 1],
+                // we need to examine the case when words are like ("apple", "app").
+                if (j >= words[i + 1].length()) return false;
+
+                if (words[i].charAt(j) != words[i + 1].charAt(j)) {
+int currentWordChar = words[i].charAt(j) - 'a';
+int nextWordChar = words[i + 1].charAt(j) - 'a';
+                    if (orderMap[currentWordChar] > orderMap[nextWordChar]) return false;
+                    // if we find the first different letter and they are sorted,
+                    // then there's no need to check remaining letters
+                    else break;
+                }
+            }
+        }
+
+        return true;
     }    
  
 
