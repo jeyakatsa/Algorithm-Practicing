211f93025a369e6244231420ffffc840b93455ba
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-08-07 20:29:36 -0700

    .

--- All-Algos/Practice5/DynamicProgramming/DynamicProgramming1.java
+++ All-Algos/Practice5/DynamicProgramming/DynamicProgramming1.java
@@ -56,5 +56,26 @@ public class DynamicProgramming1 {
         
         return maxSubarray;
     }
+
+
+    //Delete and Earn
+    public int deleteAndEarn(int[] nums) {
+        int[] count = new int[10001];
+        for (int x: nums) count[x]++;
+        int avoid = 0, using = 0, prev = -1;
+
+        for (int k = 0; k <= 10000; ++k) if (count[k] > 0) {
+            int m = Math.max(avoid, using);
+            if (k - 1 != prev) {
+                using = k * count[k] + m;
+                avoid = m;
+            } else {
+                using = k * count[k] + avoid;
+                avoid = m;
+            }
+            prev = k;
+        }
+        return Math.max(avoid, using);
+    }     
     
 }
