e093dfb470459ef33498e0e5738df23e20843078
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-08-08 18:27:45 -0700

    .

--- All-Algos/Practice5/HashTablesMap/HashTables.java
+++ All-Algos/Practice5/HashTablesMap/HashTables.java
@@ -71,8 +71,8 @@ public class HashTables {
                 if (j >= words[i + 1].length()) return false;
 
                 if (words[i].charAt(j) != words[i + 1].charAt(j)) {
-int currentWordChar = words[i].charAt(j) - 'a';
-int nextWordChar = words[i + 1].charAt(j) - 'a';
+                    int currentWordChar = words[i].charAt(j) - 'a';
+                    int nextWordChar = words[i + 1].charAt(j) - 'a';
                     if (orderMap[currentWordChar] > orderMap[nextWordChar]) return false;
                     // if we find the first different letter and they are sorted,
                     // then there's no need to check remaining letters
@@ -82,7 +82,43 @@ int nextWordChar = words[i + 1].charAt(j) - 'a';
         }
 
         return true;
-    }    
+    } 
+
+    //Insert Delete GetRandom
+    Map<Integer, Integer> dict;
+    List<Integer> list;
+    Random rand = new Random();
+    /** Initialize your data structure here. */
+    public HashTables() {
+      dict = new HashMap<>();
+      list = new ArrayList<>();
+    }
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    public boolean insert(int val) {
+      if (dict.containsKey(val)) return false;
+  
+      dict.put(val, list.size());
+      list.add(list.size(), val);
+      return true;
+    }
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    public boolean remove(int val) {
+      if (! dict.containsKey(val)) return false;
+  
+      // move the last element to the place idx of the element to delete
+      int lastElement = list.get(list.size() - 1);
+      int idx = dict.get(val);
+      list.set(idx, lastElement);
+      dict.put(lastElement, idx);
+      // delete the last element
+      list.remove(list.size() - 1);
+      dict.remove(val);
+      return true;
+    }
+    /** Get a random element from the set. */
+    public int getRandom() {
+      return list.get(rand.nextInt(list.size()));
+    }       
  
 
 
