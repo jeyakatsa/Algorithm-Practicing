b3f84edd7ad157cf5bfa51401e31880119fde9fe
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-08-15 18:40:37 -0700

    .

--- All-Algos/Practice5/Arrays/Arrays10.java
+++ All-Algos/Practice5/Arrays/Arrays10.java
@@ -208,7 +208,6 @@ public class Arrays10 {
           startPointer += 1;
     
         }
-    
         return usedRooms;
     }    
 
@@ -237,7 +236,25 @@ public class Arrays10 {
                     ++lo;
             }
         }
+    }
+    
+    //K Closest Points to Origin
+    public int[][] kClosest(int[][] points, int K) {
+        if(K == points.length) return points;
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>(K, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return (b[0]*b[0] + b[1]*b[1]) - (a[0]*a[0] + a[1]*a[1]);
+            }
+        });
+        
+        for(int[] point: points) {
+            pq.add(point);
+            if(pq.size() > K) pq.poll();
+        }
+        return pq.toArray(new int[0][0]);
     }    
+    
 
     
 }
