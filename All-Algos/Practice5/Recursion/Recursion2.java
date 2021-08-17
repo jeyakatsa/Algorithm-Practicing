9c43d3c036da447e0aa417758ece1100243a887a
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-08-16 17:31:43 -0700

    .

--- All-Algos/Practice5/Recursion/Recursion2.java
+++ All-Algos/Practice5/Recursion/Recursion2.java
@@ -68,7 +68,36 @@ public class Recursion2 {
 
         // Now the head is the second node
         return secondNode;
-    }   
+    } 
+    
+    
+    //IS PALINDROME
+    public boolean isPalindrome(TreeNode head) {
+        List<Integer> vals = new ArrayList<>();
+
+        // Convert LinkedList into ArrayList.
+        TreeNode currentNode = head;
+        while (currentNode != null) {
+            vals.add(currentNode.val);
+            currentNode = currentNode.next;
+        }
+
+        // Use two-pointer technique to check for palindrome.
+        int front = 0;
+        int back = vals.size() - 1;
+        while (front < back) {
+            // Note that we must use ! .equals instead of !=
+            // because we are comparing Integer, not int.
+            if (!vals.get(front).equals(vals.get(back))) {
+                return false;
+            }
+            front++;
+            back--;
+        }
+        return true;
+    }    
+
+
 
 
 }
