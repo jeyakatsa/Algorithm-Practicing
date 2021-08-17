c7cc0c565718f7980286d64430f77dcb5c835c22
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-08-16 18:28:06 -0700

    .

--- All-Algos/Practice5/Recursion/Recursion2.java
+++ All-Algos/Practice5/Recursion/Recursion2.java
@@ -95,6 +95,19 @@ public class Recursion2 {
             back--;
         }
         return true;
+    } 
+    
+    
+    //Reverse String
+    public void helper(char[] s, int left, int right) {
+        if (left >= right) return;
+        char tmp = s[left];
+        s[left++] = s[right];
+        s[right--] = tmp;
+        helper(s, left, right);
+    }
+    public void reverseString(char[] s) {
+        helper(s, 0, s.length - 1);
     }    
 
 
