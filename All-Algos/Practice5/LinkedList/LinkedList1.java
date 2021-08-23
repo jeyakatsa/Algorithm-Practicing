33a3fd2ecd81c6cf239e218a6396eb1db5acdf5d
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-08-22 18:55:29 -0700

    .

--- All-Algos/Practice5/LinkedList/LinkedList1.java
+++ All-Algos/Practice5/LinkedList/LinkedList1.java
@@ -120,41 +120,41 @@ public class LinkedList1 {
         }
     }
 
-    ////Reverse Linked List II
-    // public ListNode reverseBetween(ListNode head, int m, int n) {
-    //     // Empty list
-    //     if (head == null) {
-    //         return null;
-    //     }
-    //     // Move the two pointers until they reach the proper starting point
-    //     // in the list.
-    //     ListNode cur = head, prev = null;
-    //     while (m > 1) {
-    //         prev = cur;
-    //         cur = cur.next;
-    //         m--;
-    //         n--;
-    //     }
-    //     // The two pointers that will fix the final connections.
-    //     ListNode con = prev, tail = cur;
-    //     // Iteratively reverse the nodes until n becomes 0.
-    //     ListNode third = null;
-    //     while (n > 0) {
-    //         third = cur.next;
-    //         cur.next = prev;
-    //         prev = cur;
-    //         cur = third;
-    //         n--;
-    //     }
-    //     // Adjust the final connections as explained in the algorithm
-    //     if (con != null) {
-    //         con.next = prev;
-    //     } else {
-    //         head = prev;
-    //     }
-    //     tail.next = cur;
-    //     return head;
-    // }
+    //Reverse Linked List II
+    public Node reverseBetween(Node head, int m, int n) {
+        // Empty list
+        if (head == null) {
+            return null;
+        }
+        // Move the two pointers until they reach the proper starting point
+        // in the list.
+        Node cur = head, prev = null;
+        while (m > 1) {
+            prev = cur;
+            cur = cur.next;
+            m--;
+            n--;
+        }
+        // The two pointers that will fix the final connections.
+        Node con = prev, tail = cur;
+        // Iteratively reverse the nodes until n becomes 0.
+        Node third = null;
+        while (n > 0) {
+            third = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = third;
+            n--;
+        }
+        // Adjust the final connections as explained in the algorithm
+        if (con != null) {
+            con.next = prev;
+        } else {
+            head = prev;
+        }
+        tail.next = cur;
+        return head;
+    }
 
 
 
@@ -216,7 +216,22 @@ public class LinkedList1 {
         public boolean isFull() {
           return (this.count == this.capacity);
         }
-      }  
+    }  
+
+
+
+    //Intersection of Two Linked Lists
+    public Node getIntersectionNode(Node headA, Node headB) {
+        while (headA != null) {
+            Node pB = headB;
+            while (pB != null) {
+                if (headA == pB) return headA;
+                pB = pB.next;
+            }
+            headA = headA.next;
+        }
+        return null;
+    }
 
     
 
