f891778269fc87ed459b2c386c680db361e28f71
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-08-24 17:33:44 -0700

    .

--- All-Algos/Practice5/Trees/Trees.java
+++ All-Algos/Practice5/Trees/Trees.java
@@ -30,159 +30,40 @@ public class Trees {
         }
     }
 
-    //Count Good Nodes in Binary Tree
-    private int numGoodNodes = 0;
-    public int goodNodes(TreeNode root) {
-        dfs(root, Integer.MIN_VALUE);
-        return numGoodNodes;
-    }
-    private void dfs(TreeNode node, int maxSoFar) {
-        if (maxSoFar <= node.val) {
-            numGoodNodes++;
-        }
+    //Binary Tree Right Side Search
+    public List<Integer> rightSideView(TreeNode root) {
+        if (root == null) return new ArrayList<Integer>();
         
-        if (node.right != null) {
-            dfs(node.right, Math.max(node.val, maxSoFar));
-        }
-
-        if (node.left != null) {
-            dfs(node.left, Math.max(node.val, maxSoFar));
-        }
-    }
-
-    //Flatten Nested List Iterator   
-    // public class NestedIterator implements Iterator<Integer> {
+        ArrayDeque<TreeNode> nextLevel = new ArrayDeque<>() {{ offer(root); }};
+        ArrayDeque<TreeNode> currLevel = new ArrayDeque<>();        
+        List<Integer> rightside = new ArrayList<>();
         
-    //     private List<Integer> integers = new ArrayList<Integer>();
-    //     private int position = 0; // Pointer to next integer to return.
-        
-    //     public NestedIterator(List<NestedInteger> nestedList) {
-    //         flattenList(nestedList);
-    //     }
+        TreeNode node = null;
+        while (!nextLevel.isEmpty()) {
+            // prepare for the next level
+            currLevel = nextLevel.clone();
+            nextLevel.clear();
 
-    //     // Recursively unpacks a nested list in DFS order.
-    //     private void flattenList(List<NestedInteger> nestedList) {
-    //         for (NestedInteger nestedInteger : nestedList) {
-    //             if (nestedInteger.isInteger()) {
-    //                 integers.add(nestedInteger.getInteger());
-    //             } else {
-    //                 flattenList(nestedInteger.getList());
-    //             }
-    //         }
-    //     }
-        
-    //     @Override
-    //     public Integer next() {
-    //         // As per Java specs, we should throw an exception if no more ints.
-    //         if (!hasNext()) throw new NoSuchElementException();
-    //         // Return int at current position, and then *after*, increment position.
-    //         return integers.get(position++);
-    //     }
+            while (! currLevel.isEmpty()) {
+                node = currLevel.poll();
 
-    //     @Override
-    //     public boolean hasNext() {
-    //         return position < integers.size();
-    //     }
-    // }  
-
-
-    //Lowest Common Ancestor of a Binary Tree
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-
-        // Stack for tree traversal
-        Deque<TreeNode> stack = new ArrayDeque<>();
-
-        // HashMap for parent pointers
-        Map<TreeNode, TreeNode> parent = new HashMap<>();
-
-        parent.put(root, null);
-        stack.push(root);
-
-        // Iterate until we find both the nodes p and q
-        while (!parent.containsKey(p) || !parent.containsKey(q)) {
-
-            TreeNode node = stack.pop();
-
-            // While traversing the tree, keep saving the parent pointers.
-            if (node.left != null) {
-                parent.put(node.left, node);
-                stack.push(node.left);
+                // add child nodes of the current level
+                // in the queue for the next level
+                if (node.left != null) 
+                    nextLevel.offer(node.left);    
+                if (node.right != null) 
+                    nextLevel.offer(node.right);
             }
-            if (node.right != null) {
-                parent.put(node.right, node);
-                stack.push(node.right);
-            }
+            
+            // The current level is finished.
+            // Its last element is the rightmost one.
+            if (currLevel.isEmpty()) 
+                rightside.add(node.val);    
         }
-
-        // Ancestors set() for node p.
-        Set<TreeNode> ancestors = new HashSet<>();
-
-        // Process all ancestors for node p using parent pointers.
-        while (p != null) {
-            ancestors.add(p);
-            p = parent.get(p);
-        }
-
-        // The first ancestor of q which appears in
-        // p's ancestor set() is their lowest common ancestor.
-        while (!ancestors.contains(q))
-            q = parent.get(q);
-        return q;
-    }
-
-
-    //All Nodes Distance K in Binary Tree
-    Map<TreeNode, TreeNode> parent;
-    public List<Integer> distanceK(TreeNode root, TreeNode target, int K) {
-        parent = new HashMap<>();
-        dfs(root, null);
-
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(null);
-        queue.add(target);
-
-        Set<TreeNode> seen = new HashSet<>();
-        seen.add(target);
-        seen.add(null);
-
-        int dist = 0;
-        while (!queue.isEmpty()) {
-            TreeNode node = queue.poll();
-            if (node == null) {
-                if (dist == K) {
-                    List<Integer> ans = new ArrayList<>();
-                    for (TreeNode n: queue)
-                        ans.add(n.val);
-                    return ans;
-                }
-                queue.offer(null);
-                dist++;
-            } else {
-                if (!seen.contains(node.left)) {
-                    seen.add(node.left);
-                    queue.offer(node.left);
-                }
-                if (!seen.contains(node.right)) {
-                    seen.add(node.right);
-                    queue.offer(node.right);
-                }
-                TreeNode par = parent.get(node);
-                if (!seen.contains(par)) {
-                    seen.add(par);
-                    queue.offer(par);
-                }
-            }
-        }
-
-        return new ArrayList<Integer>();
-    }
-    public void dfs(TreeNode node, TreeNode par) {
-        if (node != null) {
-            parent.put(node, par);
-            dfs(node.left, node);
-            dfs(node.right, node);
-        }
+        return rightside;
     }    
+
+ 
     
     
 
