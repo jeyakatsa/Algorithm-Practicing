f014ae5a2d298051d1dcde9812f773ee656f174d
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-08-26 17:30:50 -0700

    .

--- All-Algos/Practice5/Trees/Trees.java
+++ All-Algos/Practice5/Trees/Trees.java
@@ -72,9 +72,44 @@ public class Trees {
         while (p1 != p2) {
             p1 = p1 == null ? q : p1.parent;
             p2 = p2 == null ? p : p2.parent;    
-         }
-         return p1;
-    }  
+        }
+        return p1;
+    }
+
+
+    //Validate Binary Search Tree
+    private Deque<TreeNode> stack = new LinkedList<>();
+    private Deque<Integer> upperLimits = new LinkedList<>();
+    private Deque<Integer> lowerLimits = new LinkedList<>();
+
+    public void update(TreeNode root, Integer low, Integer high) {
+        stack.add(root);
+        lowerLimits.add(low);
+        upperLimits.add(high);
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        Integer low = null, high = null, val;
+        update(root, low, high);
+
+        while (!stack.isEmpty()) {
+            root = stack.poll();
+            low = lowerLimits.poll();
+            high = upperLimits.poll();
+
+            if (root == null) continue;
+            val = root.val;
+            if (low != null && val <= low) {
+                return false;
+            }
+            if (high != null && val >= high) {
+                return false;
+            }
+            update(root.right, val, high);
+            update(root.left, low, val);
+        }
+        return true;
+    }      
 
  
     
