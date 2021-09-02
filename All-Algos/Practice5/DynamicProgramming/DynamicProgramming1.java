1bb539e40c0f7cd80bd8373fcbdd01e11da723fa
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-09-01 17:50:25 -0700

    .

--- All-Algos/Practice5/DynamicProgramming/DynamicProgramming1.java
+++ All-Algos/Practice5/DynamicProgramming/DynamicProgramming1.java
@@ -115,7 +115,6 @@ public class DynamicProgramming1 {
             generateAll(current, pos+1, result);
         }
     }
-
     public boolean valid(char[] current) {
         int balance = 0;
         for (char c: current) {
@@ -124,6 +123,27 @@ public class DynamicProgramming1 {
             if (balance < 0) return false;
         }
         return (balance == 0);
+    }  
+    
+    
+
+    //Coin Change
+    public int coinChange(int[] coins, int amount) {
+        if (amount < 1) return 0;
+        return coinChange(coins, amount, new int[amount]);
+    }
+    private int coinChange(int[] coins, int rem, int[] count) {
+        if (rem < 0) return -1;
+        if (rem == 0) return 0;
+        if (count[rem - 1] != 0) return count[rem - 1];
+        int min = Integer.MAX_VALUE;
+        for (int coin : coins) {
+            int res = coinChange(coins, rem - coin, count);
+            if (res >= 0 && res < min)
+            min = 1 + res;
+        }
+        count[rem - 1] = (min == Integer.MAX_VALUE) ? -1 : min;
+        return count[rem - 1];
     }    
     
 }
