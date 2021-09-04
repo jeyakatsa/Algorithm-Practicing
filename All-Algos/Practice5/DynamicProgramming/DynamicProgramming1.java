e9bdbe454aeb95eb4216bb8fb5cbae2174fdfc3b
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-09-04 15:29:02 -0700

    .

--- All-Algos/Practice5/DynamicProgramming/DynamicProgramming1.java
+++ All-Algos/Practice5/DynamicProgramming/DynamicProgramming1.java
@@ -161,4 +161,17 @@ public class DynamicProgramming1 {
         return dp[n];
     }
 
+
+    //Integer Break
+    public int integerBreak(int n) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        for(int i = 2; i <= n; i ++) {
+            for(int j = 1; j < i; j ++) {
+                dp[i] = Math.max(dp[i], (Math.max(j,dp[j])) * (Math.max(i - j, dp[i - j])));
+            }
+        }
+        return dp[n];
+    }    
+
 }
