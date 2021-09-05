b404347e1d1dcb3f7700da285a291e7858637a20
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-09-05 14:31:51 -0700

    .

--- All-Algos/Practice5/HashTablesMap/HashTables1.java
+++ All-Algos/Practice5/HashTablesMap/HashTables1.java
@@ -2,196 +2,24 @@ import java.util.*;
 
 public class HashTables1 {
 
-
-    //GROUP ANAGRAMS...    
-    public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String,List<String>> map = new HashMap<>();
-        for(String str : strs){
-            char[] c= str.toCharArray();
-            Arrays.sort(c);
-            String temp = Arrays.toString(c);
-            if(!map.containsKey(temp)){
-                map.put(temp,new LinkedList<>());
-            }
-            map.get(temp).add(str);
-        }
-
-        return new ArrayList<>(map.values());
-    }
-
-
-    //DAILY TEMPERATURES!
-    public int[] dailyTemperatures(int[] temperatures) {
-        Stack<Integer> stack = new Stack<>();
-        int[] ret = new int[temperatures.length];
-        for(int i = 0; i < temperatures.length; i++) {
-            while(!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
-                int idx = stack.pop();
-                ret[idx] = i - idx;
-            }
-            stack.push(i);
-        }
-        return ret;
-    }
-    
-    
-    //TOP K FREQUENT ELEMENTS
-    public int[] topKFrequent(int[] nums, int k) {
-        // O(1) time
-        if (k == nums.length) {
-            return nums;
-        }
-        
-        // 1. build hash map : character and how often it appears
-        // O(N) time
-        Map<Integer, Integer> count = new HashMap<>();
-        for (int n: nums) {
-          count.put(n, count.getOrDefault(n, 0) + 1);
-        }
-
-        // init heap 'the less frequent element first'
-        Queue<Integer> heap = new PriorityQueue<>(
-            (n1, n2) -> count.get(n1) - count.get(n2));
-
-        // 2. keep k top frequent elements in the heap
-        // O(N log k) < O(N log N) time
-        for (int n: count.keySet()) {
-          heap.add(n);
-          if (heap.size() > k) heap.poll();    
-        }
-
-        // 3. build an output array
-        // O(k log k) time
-        int[] top = new int[k];
-        for(int i = k - 1; i >= 0; --i) {
-            top[i] = heap.poll();
-        }
-        return top;
-    }  
-
-
-
-    //ANALYZE USER WEBSITE VISIT PATTERN
-    class Pair {
-        int time;
-        String web;
-        public Pair(int time, String web) {
-            this.time = time;
-            this.web = web;
-        }
-    }
-    public List<String> mostVisitedPattern(String[] username, int[] timestamp, String[] website) {
-        Map<String, List<Pair>> map = new HashMap<>();
-        int n = username.length;
-        // collect the website info for every user, key: username, value: (timestamp, website)
-        for (int i = 0; i < n; i++) {
-            map.putIfAbsent(username[i], new ArrayList<>());
-            map.get(username[i]).add(new Pair(timestamp[i], website[i]));
-        }
-        // count map to record every 3 combination occuring time for the different user.
-        Map<String, Integer> count = new HashMap<>();
-        String res = "";
-        for (String key : map.keySet()) {
-            Set<String> set = new HashSet<>();
-            // this set is to avoid visit the same 3-seq in one user
-            List<Pair> list = map.get(key);
-            Collections.sort(list, (a, b)->(a.time - b.time)); // sort by time
-            // brutal force O(N ^ 3)
-            for (int i = 0; i < list.size(); i++) {
-                for (int j = i + 1; j < list.size(); j++) {
-                    for (int k = j + 1; k < list.size(); k++) {
-                        String str = list.get(i).web + " " + list.get(j).web + " " + list.get(k).web;
-                        if (!set.contains(str)) {
-                            count.put(str, count.getOrDefault(str, 0) + 1);
-                            set.add(str);
-                        }
-                        if (res.equals("") || count.get(res) < count.get(str) || (count.get(res) == count.get(str) && res.compareTo(str) > 0)) {
-                            // make sure the right lexi order
-                            res = str;
-                        }
-                    }
-                }
-            }
-        }
-        // grab the right answer
-        String[] r = res.split(" ");
-        List<String> result = new ArrayList<>();
-        for (String str : r) {
-            result.add(str);
-        }
-        return result;
-    }
-
-
-    
-    //SHORTEST WORD DISTANCE II
-    HashMap<String, ArrayList<Integer>> locations;
-    public void WordDistance(String[] words) {
-        this.locations = new HashMap<String, ArrayList<Integer>>();
-
-        // Prepare a mapping from a word to all it's locations (indices).
-        for (int i = 0; i < words.length; i++) {
-            ArrayList<Integer> loc = this.locations.getOrDefault(words[i], new ArrayList<Integer>());
-            loc.add(i);
-            this.locations.put(words[i], loc);
-        }
-    }
-    public int shortest(String word1, String word2) {
-        ArrayList<Integer> loc1, loc2;
-
-        // Location lists for both the words
-        // the indices will be in SORTED order by default
-        loc1 = this.locations.get(word1);
-        loc2 = this.locations.get(word2);
-
-        int l1 = 0, l2 = 0, minDiff = Integer.MAX_VALUE;
-        while (l1 < loc1.size() && l2 < loc2.size()) {
-            minDiff = Math.min(minDiff, Math.abs(loc1.get(l1) - loc2.get(l2)));
-            if (loc1.get(l1) < loc2.get(l2)) {
-                l1++;
-            } else {
-                l2++;
+    //Subdomain Visit Count  
+    public List<String> subdomainVisits(String[] cpdomains) {
+        Map<String, Integer> counts = new HashMap<>();
+        for (String domain: cpdomains) {
+            String[] cpinfo = domain.split("\\s+");
+            String[] frags = cpinfo[1].split("\\.");
+            int count = Integer.valueOf(cpinfo[0]);
+            String cur = "";
+            for (int i = frags.length - 1; i >= 0; --i) {
+                cur = frags[i] + (i < frags.length - 1 ? "." : "") + cur;
+                counts.put(cur, counts.getOrDefault(cur, 0) + count);
             }
         }
 
-        return minDiff;
-    }    
-
-
- //TOP K FREQUENT WORDS [O N log N]
- public List<String> topKFrequent(String[] words, int k) {
-    // Hande null data and edge cases
-    if (words == null | words.length == 0) return new ArrayList<>();
-    
-    // Use a map to keep track of counts
-    Map<String, Integer> map = new HashMap<>();
-    // Use a list to keep track of words, to then sort them
-    List<String> list = new ArrayList<>();
-    
-    for (String word : words) {
-        // Count the # of times a words appears
-        map.put(word, map.getOrDefault(word, 0) + 1);
-        // Load only unique words to list
-        if (!list.contains(word)) 
-            list.add(word);
+        List<String> ans = new ArrayList<>();
+        for (String dom: counts.keySet())
+            ans.add("" + counts.get(dom) + " " + dom);
+        return ans;
     }
-    
-    Collections.sort(list, (String a, String b) -> {
-            int aCount = map.get(a);
-            int bCount = map.get(b);
-        
-            // If the counts are equal, then use String.compareTo to lexigraphically compare the strings
-            if (aCount == bCount) {
-                return a.compareTo(b);
-            } else {  // Else sort by greatest count
-                return bCount - aCount;
-            }
-        }
-    );
-    
-    // Return a list with only up to k elements
-    return list.subList(0, k);
-}   
-
 
 }
