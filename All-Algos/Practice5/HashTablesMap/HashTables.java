fa86d0b5f44ab61be53638f96c540d9a7c867b48
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-09-04 18:31:15 -0700

    .

--- All-Algos/Practice5/HashTablesMap/HashTables.java
+++ All-Algos/Practice5/HashTablesMap/HashTables.java
@@ -134,7 +134,114 @@ public class HashTables {
             ans.get(key).add(s);
         }
         return new ArrayList<>(ans.values());
-    }    
+    }   
+    
+    //Design Underground System
+    class UndergroundSystem {
+        Map<String, Integer> totalTravels = new HashMap<>();// keeps count of travel from x-> y
+        Map<String, Long> totalTimeStay = new HashMap<>();// keeps time stay from x-> y
+        Map<Integer, String> stationName = new HashMap<>();//keeps stationName status of customer ID
+        Map<Integer, Integer> time = new HashMap<>();//keeps checkin time of customer ID
+    
+        public UndergroundSystem() {
+    
+        }
+    
+        public void checkIn(int id, String stationName, int t) {
+            this.stationName.put(id, stationName);// where is custID
+            time.put(id, t);// put check in time
+        }
+    
+        public void checkOut(int id, String stationName, int t) {
+            String code = this.stationName.get(id) + "*" + stationName;// custID start-end
+            int cha = t - time.get(id);
+            this.stationName.remove(id);
+            time.remove(id);
+            if (!totalTravels.containsKey(code)) //travelled 1st time
+                totalTravels.put(code, 0);
+            totalTravels.put(code, totalTravels.get(code) + 1);// update total 
+            if (!totalTimeStay.containsKey(code)) 
+                totalTimeStay.put(code, 0L);
+            totalTimeStay.put(code, totalTimeStay.get(code) + cha);
+        }
+    
+        public double getAverageTime(String startStation, String endStation) {
+            String code = startStation + "*" + endStation;
+            double x = totalTimeStay.get(code);
+            return x / totalTravels.get(code);
+        }
+    }
+
+    //Time Based Key-Value Store
+    class TimeMap {
+        Map<String,Queue<TimeStamp>> map;
+       /** Initialize your data structure here. */
+       public TimeMap() {
+           map = new HashMap<>();
+       }
+       
+       public void set(String key, String value, int timestamp) {
+           //instantiate a timestamp object with the time and the value.
+           TimeStamp ts = new TimeStamp(timestamp,value);
+           if(map.containsKey(key)){
+               Queue<TimeStamp> current = map.get(key);
+               //if we have seen this key before at to its queue.
+               current.add(ts);
+               map.put(key,current);
+           }
+           else{
+               //if we havent seen it, add it and create a new queue with its value.
+               //default ordering of priority queue in java is minheap, use collections to fix this.
+               Queue<TimeStamp> current = new PriorityQueue<>(Collections.reverseOrder());
+               current.add(ts);
+               map.put(key, current);
+           }
+       }
+       
+       public String get(String key, int timestamp) {
+           //this key is not found.
+           if(!map.containsKey(key)){
+               return "";
+           }
+           else{
+               Queue<TimeStamp> current = map.get(key);
+               List<TimeStamp> copy = new ArrayList<>();
+               //search for greatest timestamp less than or equal to the one we are looking for.
+               while(!current.isEmpty() && current.peek().time > timestamp){
+                   //copy the elements that we have popped to re add them later.
+                   copy.add(current.poll());
+               }
+               //while loop breaks at first time stamp less than or equal to our timestamp. return current.peek UNLESS the queue is empty which means every time stamp 
+               //was greater. In this case return empty string.
+               String val = current.isEmpty() ? "" : current.peek().value;
+               //add back values we popped in our search.
+               current.addAll(copy);
+               return val;
+           }
+           
+       }
+   }
+   class TimeStamp implements Comparable<TimeStamp>{
+       //timestamp object maintains both time and value.
+       public int time; 
+       public String value;
+       TimeStamp(int time, String value){
+         this.time=time;
+         this.value=value;
+       }
+       //compare the nodes based on time so that the heap will be ordered correctly. This is an interface in Java, if not clear research comparable interface.
+       public int compareTo(TimeStamp t ){
+           if(this.time==t.time){
+               return 0;
+           }
+           else{
+               return this.time > t.time ? 1 : -1;
+           }
+       }
+           
+   }   
+    
+
  
 
 
