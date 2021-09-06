2c667818712e479b11e7345be1f0c45e54307549
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-09-05 18:34:25 -0700

    .

--- All-Algos/Practice5/Arrays/Arrays2.java
+++ All-Algos/Practice5/Arrays/Arrays2.java
@@ -31,4 +31,22 @@ public class Arrays2 {
         nums[j] = temp;
     }  
 
+
+    //Kth Largest Element
+    public int findKthLargest(int[] nums, int k) {
+        // init heap 'the smallest element first'
+        PriorityQueue<Integer> heap =
+            new PriorityQueue<Integer>((n1, n2) -> n1 - n2);
+
+        // keep k largest elements in the heap
+        for (int n: nums) {
+          heap.add(n);
+          if (heap.size() > k)
+            heap.poll();
+        }
+
+        // output
+        return heap.poll();        
+  }    
+
 }
