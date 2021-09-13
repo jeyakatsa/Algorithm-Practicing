f12ba3affb8cb45a5f7283e0ae380f3d0af9606b
Jeremy Akatsa <jeyakatsa@gmail.com>
2021-09-12 17:31:17 -0700

    .

--- All-Algos/Practice5/Trees/Trees1.java
+++ All-Algos/Practice5/Trees/Trees1.java
@@ -145,7 +145,63 @@ public class Trees1 {
             return null;
         InOrder(root);
         return balancedBST(0,list.size()-1);
-    }  
+    } 
+    
+    
+    //Boundary of Binary Tree
+    public boolean isLeaf(Node t) {
+        return t.left == null && t.right == null;
+    }
+    public void addLeaves(List<Integer> res, Node root) {
+        if (isLeaf(root)) {
+            res.add(root.val);
+        } else {
+            if (root.left != null) {
+                addLeaves(res, root.left);
+            }
+            if (root.right != null) {
+                addLeaves(res, root.right);
+            }
+        }
+    }
+    public List<Integer> boundaryOfBinaryTree(Node root) {
+        ArrayList<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        if (!isLeaf(root)) {
+            res.add(root.val);
+        }
+        Node t = root.left;
+        while (t != null) {
+            if (!isLeaf(t)) {
+                res.add(t.val);
+            }
+            if (t.left != null) {
+                t = t.left;
+            } else {
+                t = t.right;
+            }
+
+        }
+        addLeaves(res, root);
+        Stack<Integer> s = new Stack<>();
+        t = root.right;
+        while (t != null) {
+            if (!isLeaf(t)) {
+                s.push(t.val);
+            }
+            if (t.right != null) {
+                t = t.right;
+            } else {
+                t = t.left;
+            }
+        }
+        while (!s.empty()) {
+            res.add(s.pop());
+        }
+        return res;
+    }    
 
 
 
