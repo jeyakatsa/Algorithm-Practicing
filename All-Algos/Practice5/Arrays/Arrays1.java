70597fbfeabed811b6b8c1653f8135612647eb44
Jeremy Akatsa <jeyakatsa@gmail.com>
2022-03-20 18:52:10 -0700

    Algorithm Practicing

--- All-Algos/Practice5/Arrays/Arrays1.java
+++ All-Algos/Practice5/Arrays/Arrays1.java
@@ -24,5 +24,28 @@ public class Arrays1 {
     }
     //if including HashMap, then can separate for loops into solution O(n)
 
+
+    //LRU Cache
+    class LRUCache extends LinkedHashMap<Integer, Integer>{
+        private int capacity;
+        
+        public LRUCache(int capacity) {
+            super(capacity, 0.75F, true);
+            this.capacity = capacity;
+        }
+    
+        public int get(int key) {
+            return super.getOrDefault(key, -1);
+        }
+    
+        public void put(int key, int value) {
+            super.put(key, value);
+        }
+    
+        @Override
+        protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+            return size() > capacity; 
+        }
+    }
   
 }
