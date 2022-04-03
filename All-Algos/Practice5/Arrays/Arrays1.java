5c1b28630438cd642316a37bb71226ff16f3f475
Jeremy Akatsa <jeyakatsa@gmail.com>
2022-04-03 15:50:43 -0700

    Continuing Algos

--- All-Algos/Practice5/Arrays/Arrays1.java
+++ All-Algos/Practice5/Arrays/Arrays1.java
@@ -46,7 +46,23 @@ public class Arrays1 {
 
 
     //Number of Islands
-    public int numIslands(char[][] grid) {
+    // O(MxN)
+    void dfs(char[][] grid, int r, int c) {
+        int nr = grid.length;
+        int nc = grid[0].length;
+    
+        if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
+          return;
+        }
+    
+        grid[r][c] = '0';
+        dfs(grid, r - 1, c);
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c - 1);
+        dfs(grid, r, c + 1);
+      }
+    
+      public int numIslands(char[][] grid) {
         if (grid == null || grid.length == 0) {
           return 0;
         }
@@ -54,40 +70,16 @@ public class Arrays1 {
         int nr = grid.length;
         int nc = grid[0].length;
         int num_islands = 0;
-    
         for (int r = 0; r < nr; ++r) {
           for (int c = 0; c < nc; ++c) {
             if (grid[r][c] == '1') {
               ++num_islands;
-              grid[r][c] = '0'; // mark as visited
-              Queue<Integer> neighbors = new LinkedList<>();
-              neighbors.add(r * nc + c);
-              while (!neighbors.isEmpty()) {
-                int id = neighbors.remove();
-                int row = id / nc;
-                int col = id % nc;
-                if (row - 1 >= 0 && grid[row-1][col] == '1') {
-                  neighbors.add((row-1) * nc + col);
-                  grid[row-1][col] = '0';
-                }
-                if (row + 1 < nr && grid[row+1][col] == '1') {
-                  neighbors.add((row+1) * nc + col);
-                  grid[row+1][col] = '0';
-                }
-                if (col - 1 >= 0 && grid[row][col-1] == '1') {
-                  neighbors.add(row * nc + col-1);
-                  grid[row][col-1] = '0';
-                }
-                if (col + 1 < nc && grid[row][col+1] == '1') {
-                  neighbors.add(row * nc + col+1);
-                  grid[row][col+1] = '0';
-                }
-              }
+              dfs(grid, r, c);
             }
           }
         }
     
         return num_islands;
-      }
+    }
   
 }
